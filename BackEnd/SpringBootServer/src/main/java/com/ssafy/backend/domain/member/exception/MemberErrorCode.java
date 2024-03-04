2e97a8e47037edc178641a9d2bc1def35b08b5da
신동근 <tlsehdrms124@naver.com>
2024-03-05 00:41:11 +0900

    feat: 회원 관련 커스텀 Exception 처리
    Former-commit-id: 501f195be323b0321c61dc1de51f8d745058b276

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/exception/MemberErrorCode.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6cfd5617c059701ab60e40155c80d79cd197dc30b794923d158c2a8fb865ae1f
+size 607
---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/exception/MemberExceptionHandler.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:84e7c4eb3cdc142e6d88f97f8998dc4c986f6a8b1b1d1a63a06f98a8c3e2e174
+size 1723
