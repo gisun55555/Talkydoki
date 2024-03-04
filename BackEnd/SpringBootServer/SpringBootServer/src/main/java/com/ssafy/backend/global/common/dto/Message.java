cd03417e37bd602e78d1722290d2a203dff00495
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-04 17:16:17 +0900

    feat: BaseEntity 설정 및 클라이언트단에 보내줄 dto response 설정

---
+++ BackEnd/SpringBootServer/SpringBootServer/src/main/java/com/ssafy/backend/global/common/dto/Message.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e23a4bfd261a45cc87f3a100cf8a9be8c6751f8393262830db86b5b6aed727a7
+size 3976
---
+++ BackEnd/SpringBootServer/SpringBootServer/src/main/java/com/ssafy/backend/global/common/entity/BaseEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:027b5da8b1a118163ef3395b588a752d8199d01ef67342b988f1997b6b0c478b
+size 1447
