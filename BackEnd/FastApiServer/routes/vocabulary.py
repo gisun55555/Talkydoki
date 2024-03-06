723689626cb6e0d5e5b9b222055f802af8e29617
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-06 14:19:24 +0900

    feat: 동적 페이지 크롤링 세팅
    Former-commit-id: 64d921d1594d15a01df7a6370fc346caee8486b2

--- BackEnd/FastApiServer/routes/vocabulary.py
+++ BackEnd/FastApiServer/routes/vocabulary.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e83217f314d2ddc640a04f5a16a5eff916f70b5a5433633bcde2faaa103a2c1e
-size 1367
+oid sha256:3ca374759ec37e4ee41499e32c09fa3710feab82bb857280c0307798d58f9fe6
+size 1954
