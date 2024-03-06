b18588205f1232c3368d79d6d2b084c365891cb1
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-06 14:59:28 +0900

    feat: 네이버 일본어 사전 크롤링 작업 후 db에 저장 기능 개발
    Former-commit-id: 14df13d1b8f60d5d277df516ae9c743de6da4212

--- BackEnd/FastApiServer/routes/vocabulary.py
+++ BackEnd/FastApiServer/routes/vocabulary.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ca374759ec37e4ee41499e32c09fa3710feab82bb857280c0307798d58f9fe6
-size 1954
+oid sha256:b2d5f5f2cfb841477a663ce027477d33ac8a4d5f36e3450b09bed46d83d0f00a
+size 3499
