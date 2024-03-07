e905d3b5cde9bbb08bbd3adc2436402085f86bcc
오기선 <pakox123@gmail.com>
2024-03-07 14:51:54 +0900

    fix:프론트엔드 절대경로 추가
    Former-commit-id: 44d7c0297b8f4328735aa71f39e3246c1edb515d

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb4eaeea2a0c818d6568c9563b05ee4858008e815a342db62f6c92249ea47b0b
+oid sha256:e942cae7b9294d3b422012caf2c565499bd65f8f4a42e020a4178febd5002bfd
 size 489
