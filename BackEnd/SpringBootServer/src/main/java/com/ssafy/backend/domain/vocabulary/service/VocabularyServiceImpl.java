6971b6f8833511d314f8facccc4ea7838aa05e45
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-07 17:31:41 +0900

    feat: 오늘의 단어 가져오기 및 회원정보 가져오기 읽기전용 트랜잭션 설정
    Former-commit-id: bda263aa41b273e8949ea59419629154141e3dea

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/service/VocabularyServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/service/VocabularyServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ec1956cf08b80a81ab9c4889c9eee3d4cfc943f0060f13738dd845651a1aca3
-size 1181
+oid sha256:039aa235d24c57274876340c279b447de8df69edecda78b9fb28fae399d11167
+size 1217
