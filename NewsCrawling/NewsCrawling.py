f209f1ae7fb79d282aa93896f6ca227e1426f977
송강산 <rkdtks0816@naver.com>
2024-03-08 17:45:28 +0900

    feat : 뉴스 크롤링 완료
    Former-commit-id: 742ad9c6efddb985b94a6499bd2d8329b1590e91

--- NewsCrawling/NewsCrawling.py
+++ NewsCrawling/NewsCrawling.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f39aa8e034c86f6c76459cd0193eb6e1c0490c0615a30006295b4d8505fbc81
-size 5414
+oid sha256:8f54bd2555e16b1d0eec0c63c64055e6e864c33fbb81956fa4c2ee9929c73e6f
+size 7080
---
+++ NewsCrawling/install_python.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:85501304146b860830093ea99f3ea9585c83229bae467436b9e61a01e8f20149
+size 445
