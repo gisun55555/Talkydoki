62f578bb84b2885728d18004bf46736124a63d13
백민정 <peku9693@gmail.com>
2024-03-08 16:03:06 +0900

    fix: api 일부 수정, font 추가, css global 설정 추가
    Former-commit-id: 7d9dfd058689176a276208f4e0a02b2d6188f68d

--- FrontEnd/src/routes/Intro.tsx
+++ FrontEnd/src/routes/Intro.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5970088e1992db1e11a35cff5cd95445fb4cfb8f127e556c0ee616aa5122c9de
-size 99
+oid sha256:41ab30045bbee0eb7eb6a79250ffcd62059f4d203ea9aee80d7e81fbc4ce594c
+size 3968
