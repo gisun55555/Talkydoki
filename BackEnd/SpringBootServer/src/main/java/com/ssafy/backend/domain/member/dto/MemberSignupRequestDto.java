9e050d15e76cd894040a712902f87a1f4e7206b6
신동근 <tlsehdrms124@naver.com>
2024-03-11 12:00:17 +0900

    feat: OAUTH 도메인 설정
    Former-commit-id: fda0f50bcfd64ccffc7e3491cd6f3bdb40071210

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/dto/MemberSignupRequestDto.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/dto/MemberSignupRequestDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4421f0c2fd394c180ebc1bf75b645c8c6e4f0347842b3ef30587dec2ae36cec
-size 1489
+oid sha256:b43159c7e5f47f4d28a7d5da15d143009fb4ac13847810406fa5ce966ac972aa
+size 1485
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/exception/MemberExceptionHandler.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/exception/MemberExceptionHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84e7c4eb3cdc142e6d88f97f8998dc4c986f6a8b1b1d1a63a06f98a8c3e2e174
-size 1723
+oid sha256:54b1ef5046a4ee249f8daf9e7101a2abd96325ee0a9b05d439a614f48071e19d
+size 1726
