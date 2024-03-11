56c52d210b627779c644d7a21f687c309e741972
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-11 17:40:14 +0900

    feat: 소셜 로그인 관련 swagger 설정
    Former-commit-id: 93d36e7b7ec6acfd353c17148694682b71177044

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/controller/OAuthController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/controller/OAuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f363527be0f52c4998b7c8c78dc381d1cf8c3776b90870bc579c2157e958133c
-size 1837
+oid sha256:020027acacba0f7065b74ca6045b636a7c4e75282b4d696163cfead340b528c9
+size 2451
