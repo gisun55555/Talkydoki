d4e09cd1baf1bb87bd867702a16dfec8c8434aff
신동근 <tlsehdrms124@naver.com>
2024-03-11 14:39:56 +0900

    feat: 구글 소셜 정보 가져오기 설정 및 구글 소셜 로그인 클라이언트 빈 설정 완료
    Former-commit-id: 7d86fd8dfebb73a9152b0e139fedf5c1c35031ec

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/oauth/vendor/google/client/GoogleApiClient.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8a523c6b2fe87e03c6589d0cec58c62f6c8c413f0fe2ae070466122f6fc6bf90
+size 939
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/config/HttpInterfaceConfig.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/config/HttpInterfaceConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2e860f225767c3cdf8332d30c69994cc9ca8cf2282fddd794a3ebc5820f30f3
-size 2097
+oid sha256:141e2b487f5d25bcda0f0b1bb056824d07076c1b99b3137487b1f65b4883d221
+size 2472
