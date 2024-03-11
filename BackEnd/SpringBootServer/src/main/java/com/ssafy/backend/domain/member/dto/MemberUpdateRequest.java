210cd01af20bcea35601024b08da7c178ac715da
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-11 17:07:39 +0900

    feat: 회원정보 수정 API 개발
    Former-commit-id: fc7c4f86fe404632dd97f3722c219aede3d4cfe7

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/dto/MemberUpdateRequest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e0e8dc912922753b8021990a8881c74579927388a695337ad56a2646dfca75e5
+size 122
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/entity/Member.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:734468c0d2b607994b3ac2dbbfa852bf90ee5140d8f725de12ca09cd80747e5e
-size 1029
+oid sha256:5177db5f0c2304dce16594ee03bcd5e2213f19ed257783dde8a9cfec9137b106
+size 1291
