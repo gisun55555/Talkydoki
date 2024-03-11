a9e53139f4e0d06391c54add84b985581da2681c
신동근 <tlsehdrms124@naver.com>
2024-03-11 14:02:27 +0900

    feat: 네이버 소셜 로그인 설정 완료
    Former-commit-id: ae6f387f8afd3b6c3e35740f632d06b3e3f219dc

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/oauth/vendor/naver/authcode/NaverOAuthCodeUrlProvider.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6da88b574385bfddc30a863bc3ddccb60f4320e769980965bb1443d31264b41a
+size 1138
---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/oauth/vendor/naver/client/NaverApiClient.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bfbe1df6dbc735285e829d52fef94af0f8ec5dedd82cb6f959fce13468aab1ff
+size 1201
