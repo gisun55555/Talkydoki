e72b0b478e4080f4c15c41c8ac3dba0afeddb482
BMJ <peku9693@gmail.com>
2024-03-11 22:33:15 +0900

    fix: 로고 및 기본 프로필 이미지 색상 수정
    Former-commit-id: 7da0d0ced280afc19742d2d951bfd1e22673c37d

--- FrontEnd/src/components/Menu/Menus.tsx
+++ FrontEnd/src/components/Menu/Menus.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e02fe2b26d067a852273663524309cdb0443f24f7f35daef79e40e605087ad4
-size 1134
+oid sha256:1d92ab55fafb8282d6c90b1f65f59b9704e04b771200a3a3a472c767fb82601e
+size 1239
--- FrontEnd/src/styles/Menu/sidebar.tsx
+++ FrontEnd/src/styles/Menu/sidebar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05435752b4eca9729cd22effd5ea5cf22f3aea6174e965a28f25cbef3d040ebd
-size 2135
+oid sha256:1b3759621eeb94f3be42c974dfe8dcfe6f006a574182a60b0d2d19e59922d7e6
+size 2142
