513ba070274c9d2d0983777753da4ad568e5c7b3
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-11 17:26:18 +0900

    feat: firebase Storage 프로퍼티 설정 및 serviceKey 생성
    Former-commit-id: 6822722e3ed418de3dfad1c1ed20bfa3ab7611e5

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/config/FirebaseConfig.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/config/FirebaseConfig.java
@@ -1,4 +1,3 @@
-package com.ssafy.backend.global.config;
-
-public class FirebaseConfig {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ceb431f01d273dad083ea2ca3098054224cb8e2db1cdb79bbaa3a9db55a0a0f8
+size 2375
