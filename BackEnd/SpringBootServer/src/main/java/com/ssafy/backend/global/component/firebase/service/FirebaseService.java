892340a09be228c7f79953585a71995e79b4df50
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-11 17:12:04 +0900

    feat: Firebase 의존성 주입
    Former-commit-id: 7f969941607b351de979a47c5531883066450d57

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/firebase/service/FirebaseService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a63a9150a12f35a490acacc2bb5dce6407d171075d4f4c9a2eb0b2d914f85233
+size 216
---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/config/FirebaseConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8c193835280ad1c8fc9c902189718c9ab1860a44b8bbf659405d7932ba19cee
+size 74
