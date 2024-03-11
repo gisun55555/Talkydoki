23a099e816634f63d91b71206223f5955f41df63
Ubuntu <ubuntu@ip-172-26-11-187.ap-northeast-2.compute.internal>
2024-03-11 05:41:41 +0000

    feat: CI/CD 테스트 1
    Former-commit-id: 7d52a1f1de0dfe81329e3a2665a438fccc69633a

---
+++ CICD/Install/installJenkins.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:03f15ec536e0c9078f7f48f92345a86818a6ea9ce26a4d6b06f268b4828042fa
+size 948
---
+++ CICD/jenkins.Dockerfile
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70dbb0763980966b567df40763cd538beecae62b67d98355eb9e67e847f74caa
+size 534
