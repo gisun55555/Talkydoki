485c07e4223c6b3cb6ec3f2aeef86a40814fbea0
Ubuntu <ubuntu@ip-172-26-11-187.ap-northeast-2.compute.internal>
2024-03-11 05:41:41 +0000

    feat: CI/CD 테스트 1
    Former-commit-id: 7093bd24d2a1fdf99076bad0b4e0e6b9b53eaeca

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/dto/MemberInfo.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/dto/MemberInfo.java
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:56d3c4de641e704ec6a55d8ef6700447f5e76b54b5d496dbce5f822222bfcfa3
-size 313
+package com.ssafy.backend.domain.member.dto;
+
+import com.ssafy.backend.domain.member.entity.enums.MemberRole;
+import lombok.Builder;
+
+@Builder
+public record MemberInfo(
+        Long id,
+        String email,
+        String name,
+        String nickname,
+        String profileImage,
+        MemberRole role
+) {
+}
