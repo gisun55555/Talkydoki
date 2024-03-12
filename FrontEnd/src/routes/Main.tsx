fefa96f0213b24fafdd90a921d818adceb457bc0
송강산 <rkdtks0816@naver.com>
2024-03-12 10:31:23 +0900

    fix: 사용하지 않는 코드 삭제 Former-commit-id: 3076ea356ddb7fae8c4dea802c324047d5143dca

--- FrontEnd/src/routes/Main.tsx
+++ FrontEnd/src/routes/Main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd55330e26c9810c62813864d9d9b35f324fe865aa3c62cef73e9bcf6d915d0c
-size 160
+oid sha256:79e89605c3054506637e63766ca0f3cb5cb3eb169d8ac6910bbfc09fa144b30b
+size 133
