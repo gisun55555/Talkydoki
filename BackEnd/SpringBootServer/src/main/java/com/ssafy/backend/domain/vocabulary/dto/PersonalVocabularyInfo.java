4f735a790711dd82bfa722951dc1e8a00a21f3f8
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-12 12:12:57 +0900

    feat: QueryDSL 의존성 주입 및 설정 완료
    Former-commit-id: 9804731166962a0d2110ad29d5dcb368e0ff6c0b

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/dto/PersonalVocabularyInfo.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fd3329607058a317a7ca3f83d34062d69bd882740b045d15ec414e7958a70060
+size 226
---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/repository/PersonalVocabularyRepositoryCustom.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6d105be4e704500bca428ba75e56aa28fb552eb3885412670851d747f7813d4d
+size 214
