5047f2032b418392f1df634874d9983a1eeaf37f
이지호 <jeho1129@naver.com>
2024-03-12 17:12:36 +0900

    update: 추천 기능 데이터셋, 알고리즘 수정
    Former-commit-id: ef423947bbe6edbc07790dc207ffcf957ff1ba34

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/oauth/vendor/google/GoogleOAuthProps.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/oauth/vendor/google/GoogleOAuthProps.java
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:16b906239aaddb779bb663eb6611dcf4a00ae09b74d67d4a9f7d73dd7c51faaa
-size 335
+package com.ssafy.backend.global.component.oauth.vendor.google;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oauth.google")
+public record GoogleOAuthProps(
+        String redirectUri,
+        String clientId,
+        String clientSecret,
+        String[] scope
+) {
+}
---
+++ BackEnd/c107_recommendation_1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:031bbcfc5350896b49cd532fede770a514d92846abf2110950c5a71bdf527414
+size 13436
