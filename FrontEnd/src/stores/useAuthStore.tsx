f6a262d14691e1d5cb9df352afb5b3b295284b2c
백민정 <peku9693@gmail.com>
2024-03-12 09:40:17 +0900

    fix: MUI 다크테마 추가
    Former-commit-id: 3b964d83f2b1dba774c9ae753b1a1fad41798a58

--- FrontEnd/src/stores/useAuthStore.tsx
+++ FrontEnd/src/stores/useAuthStore.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91858e8f7a940bf4a44f086575ee33883f35212d78c9f207ebca35684d849beb
-size 910
+oid sha256:b8fc8fbe3923fb43d4e899418f4ff70882311abb376d612362798b2b96524d28
+size 1027
