47eaab67879af5eff374bf217a530c6adf6d1bbd
송강산 <rkdtks0816@naver.com>
2024-03-12 17:49:40 +0900

    feat: 작성날짜 형식 변경
    Former-commit-id: 9872f60e7624bf13ac15c8ef8b5521512afb90db

--- NewsCrawling/NewsCrawling.py
+++ NewsCrawling/NewsCrawling.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f54bd2555e16b1d0eec0c63c64055e6e864c33fbb81956fa4c2ee9929c73e6f
-size 7080
+oid sha256:b0400b8ae41eb1eba5d6de7a04968cd0883941deda27087eb4732040667cdb31
+size 6261
