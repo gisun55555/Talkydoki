0fd579bff692317abe35c2cf9f20bce69ce8d295
신동근 <tlsehdrms124@naver.com>
2024-03-13 14:04:01 +0900

    feat: 비밀번호 변경 API 기능 강화
    Former-commit-id: da3bb14e266525d0ddccd4af4f42989b0c176f99

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/dto/MemberPasswordChangeRequest.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/dto/MemberPasswordChangeRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d21d5fe47d80bc2e994582972f331d4127d06921c7cfad415ebae5e8e5f62340
-size 1251
+oid sha256:8126b669e363711c3aa5116b2340426aa3e01ed6c439f10b8e4a1838fd6af9d6
+size 699
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/service/MemberServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/service/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd76824a5cf8f21669fc5ed040bf2c583f75c65d62c4dcfa1834061f6dddf83a
-size 4707
+oid sha256:d369e31d09cb79ae2b0a24e98a6b85070d0a36e5ace9acf107c32d82efde6326
+size 4870
