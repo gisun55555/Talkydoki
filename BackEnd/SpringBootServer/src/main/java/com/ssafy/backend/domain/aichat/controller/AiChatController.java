0cf56954043ae0365d15b563df1191f86c121e27
조현호 <chh5680@naver.com>
2024-03-14 07:16:49 +0900

    refactor : Controller에서 @MessageMapping으로 로그 저장
    Former-commit-id: d1e8142c399f5cb1b7dc87822410eab9f665861e

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:000210c60311dca66c58fba6ab52643527ccc0133c0eba42514e7e5dd01d9ae8
-size 2444
+oid sha256:5e89316a80704f2f5d6b85f5433457368f0d8e11c49c8b85ac87c513c5f22bbd
+size 2545
