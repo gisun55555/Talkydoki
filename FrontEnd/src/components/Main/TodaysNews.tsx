de2858683a45b02cedd47300f6a06ad457a65d7a
백민정 <peku9693@gmail.com>
2024-03-13 09:06:39 +0900

    fix: build 오류 수정
    Former-commit-id: 953464df356fc054306f79a90fb60fb0fa4d97a3

--- FrontEnd/src/components/Main/TodaysNews.tsx
+++ FrontEnd/src/components/Main/TodaysNews.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3605932ad18fbfbe5cc92a8e3377b7d35cc73f31f6b39f65bf997a3e37b33cf4
-size 1306
+oid sha256:00ae010fe909f90d162f92df84f45b1891984c9f1533fc2fa45985fbd678f3b8
+size 1279
