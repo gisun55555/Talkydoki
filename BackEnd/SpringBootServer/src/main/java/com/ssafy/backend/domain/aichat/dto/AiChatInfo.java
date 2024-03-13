00f3c294ce3a8dcffcecf0b7b31f376b70135c9e
GroovyKim <chh5680@naver.com>
2024-03-13 16:25:21 +0900

    fix: AiChatRoom 생성
    Former-commit-id: 509ac7a7d45399ab10fb5714d5f50f6d7aee527d

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/dto/AiChatInfo.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:caef551c1398d7d39d68e596253c49a2e16ef3a4086411c117345e763b0a9a01
+size 753
---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/dto/AiChatRoomCreatedResponse.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:384fb01d9436a15915ef61db564ef8430cd59e5d34117b1ad4e6e51b737fe480
+size 541
