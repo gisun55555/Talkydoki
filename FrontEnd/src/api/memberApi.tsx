781643a6a8d8103ee27b446d05f8a924b61b0dac
오기선 <pakox123@gmail.com>
2024-03-14 14:51:48 +0900

    feat: 로그아웃 요청 추가
    Former-commit-id: 2e159c3f3ace1ced29e6e98fc64967dcff624eee

--- FrontEnd/src/api/memberApi.tsx
+++ FrontEnd/src/api/memberApi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f42cb9b4fcc4b53c65a8ccb162b73f4b4c7e4b307e380dec377e72f93da861c
-size 2054
+oid sha256:5b2e9e3de00cc840edc29ac2ea951305f758fe5114355f967375b68178e570fb
+size 2601
