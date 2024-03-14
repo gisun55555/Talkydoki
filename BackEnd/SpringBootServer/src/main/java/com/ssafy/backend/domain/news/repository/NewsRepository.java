186a0bc5b722aa8d9b0d7b2012322aa7a25fc780
송강산 <rkdtks0816@naver.com>
2024-03-14 16:22:12 +0900

    fix : 뉴스 조회 에러 수정 (임시 fetch = FetchType.EAGER)
    Former-commit-id: 25f4c163b977aa6cafc3f90dd5e829eb4ec71248

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/repository/NewsRepository.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/repository/NewsRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8ce9ecd4be5c0266896a47aceca8f0d92239708c9bb6c17e397cc75082a1a52
-size 653
+oid sha256:0c0b8dd29fab3491d34be44a01e353a865aaa142d74b46cf241b05fcdd054e73
+size 662
