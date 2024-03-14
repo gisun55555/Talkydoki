367dd492d7f7ef86b2214a49c4cf7ba5d172680f
백민정 <peku9693@gmail.com>
2024-03-14 15:35:08 +0900

    fix: 로그아웃 연동, 프로필 불러오기 기능 수정
    Former-commit-id: 2d52ee42dc0d9b43c6e2fcfbef933863771f7dcd

--- FrontEnd/src/api/memberApi.tsx
+++ FrontEnd/src/api/memberApi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f17e56ef9eb1f91d7ca6c595e888eabd7f1e66ebee3dbb23aa8bf45bde34642
-size 3737
+oid sha256:1897a8d99fc11a4f1e5a44929e871038107ff695601f046f8a7459118812d3ab
+size 3744
