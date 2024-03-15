9b7ccf0633b6d72193e964b8dacb7f648b76436a
이지호 <jeho1129@naver.com>
2024-03-15 17:04:23 +0900

    update : Keyword 저장 관련 FastAPI + SpringBoot 코드 수정
    Former-commit-id: a17599be42b11a81eca01ca72d0c7eee330add3e

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/dto/KeywordPostRequest.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/dto/KeywordPostRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:737ed3024df80dcfdb061d6940b90ebebbfa4378e327d2a8a010b257414259b8
-size 527
+oid sha256:11748de6ece2c6d0b31b64dd63da7a088bae1b997d444abc3adc8e63c8eae836
+size 579
