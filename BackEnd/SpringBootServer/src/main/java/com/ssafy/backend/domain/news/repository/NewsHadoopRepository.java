759357d72c2080dfa639fe31cee52c4d960992cb
송강산 <rkdtks0816@naver.com>
2024-03-15 16:09:55 +0900

    feat : 하둡과 프론트 나누고 프론트 뉴스 리스트 조회 기능 추가
    Former-commit-id: 1228a1fdc6e375851f5d89b6b7c14388fc0aa307

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/repository/NewsHadoopRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11ff1d2b39fe909db2cc5d5ce0d95c76cf247aa3e0b1d5c1fd9e01606229b937
+size 374
---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/NewsHadoopService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:85372d085c00f0a0f9b9b7899318c08620542d46ed49c4cf7f9e2f7429cc98f9
+size 293
