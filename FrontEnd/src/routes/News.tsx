031333c5bd78e45cc7903e36941d60cfb89091cb
송강산 <rkdtks0816@naver.com>
2024-03-15 16:30:06 +0900

    fix : FrontEnd/src/routes/News.tsx 3번 줄 주석
    Former-commit-id: a91513ce5dc2d34919abe0c16b342858c127e354

--- FrontEnd/src/routes/News.tsx
+++ FrontEnd/src/routes/News.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:977a6d28989537aff20356f239eecfbc2b57d4386a2d65da212d75f2d20b4e90
-size 309
+oid sha256:8043f8a6b26b6ab083f21e5b1dac7c30fe62f6db5f37fcaa01e3f98acdae2e52
+size 312
