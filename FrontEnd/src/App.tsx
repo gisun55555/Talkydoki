17164e767d70d5d4dbb306e4585774e545b2769c
오기선 <pakox123@gmail.com>
2024-03-15 21:15:21 +0900

    feat: ai채팅리스트 추가
    Former-commit-id: d81fa875fb3a1b3c9e75bc569b2e7b6bb8a7d270

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7b50edfb568d7c26ac421144d82be6425419435d90dfc95d40a41743aba641d
-size 2080
+oid sha256:6e0f7fbb0ba1bab702e7a26582e51990493bade81b21b9a691431f77e4f128fe
+size 2327
---
+++ FrontEnd/src/styles/Aichat/AiChatList.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9fcc3b8693b8004022ac9dd3e30be472d57a49aff49069317ec6cee48ec7c48e
+size 951
