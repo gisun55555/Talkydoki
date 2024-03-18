1bab9326e1a0370a0bde7a44fb66040641c847ab
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-18 14:50:42 +0900

    fix: 단어사전 일본어 괄호 제거
    Former-commit-id: 11f8230f0f8c3e4363206a88636943b747ca689c

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/service/VocabularyService.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/service/VocabularyService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:736295ef457acefe022f3d4a62e1b015730da9c002cc7b71a1c4968b48200d66
-size 807
+oid sha256:501f2d7a0124ba0eb66787779ac4c643913d951f0d0d00a848091e0b3235a859
+size 982
