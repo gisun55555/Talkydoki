e724e6b6572f73cd785e88f68917ec5c3865b875
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-18 11:55:24 +0900

    fix: 나만의 단어장 삭제 API 수정
    Former-commit-id: e78f93b5cf71896840612424866d7fdce4e4f7d1

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/repository/PersonalVocabularyRepository.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/repository/PersonalVocabularyRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2dac519ac7214588d84417316a844dcbd256b7fcd412058608ecefe38e21853
-size 581
+oid sha256:336f637a28e4c96df32614d86a5f572c17f2e4330fa845205760c9e8dbcad262
+size 923
