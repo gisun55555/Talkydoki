e2d6fb8ae8c679cbbb8e2efa259df623a73a5a0d
송강산 <rkdtks0816@naver.com>
2024-03-18 10:50:15 +0900

    fix : 코드 주석, 타입지정
    Former-commit-id: db905e2a72d3269a8660db364669ebb7ac1314a3

--- FrontEnd/src/api/vocaApi.tsx
+++ FrontEnd/src/api/vocaApi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e159fa2e98cc10ae3c47b803ccba64a978bfb506482123ddd19743a7ea8f2f72
-size 3352
+oid sha256:4c1d879fd31510d9a94da9d4ab0276a0da3a67885d252073b11933827978adfb
+size 3361
