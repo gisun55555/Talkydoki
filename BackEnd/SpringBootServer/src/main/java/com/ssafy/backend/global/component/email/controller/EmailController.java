019c521cd2b7944716fa3ac231617a31a5e92ff0
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-18 16:04:38 +0900

    feat: 이메일 인증코드 발송 API 개발
    Former-commit-id: 022310533025d1bcba52eded330f53cfaf740bfd

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/email/controller/EmailController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b6874a30801515847f19f6facd6f082871a4641e6507d2d309a7c36be9b1d6e
+size 1039
---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/email/service/EmailServiceImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e88a3576112b7cc2b004c5f68e60677b170108b8b90aeb8fac9f43a5dc73c87d
+size 3031
