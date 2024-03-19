0d8ccb924c9640c5b0a5816f45e24c8bab25ced6
백민정 <peku9693@gmail.com>
2024-03-19 15:17:46 +0900

    fix: 버그수정
    Former-commit-id: c1085dcd2404e73ac9d0a55f96a32815fe03e34b

--- FrontEnd/src/api/profileApi.tsx
+++ FrontEnd/src/api/profileApi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:847474e2c19e5ef3d67e6bcc5c292798717719f9c3f0e2313cb0bf2a7266f269
-size 2340
+oid sha256:3e70b85018baeee94eeb94b431591878d6beccfaa6ef605de7d6da04bfe78ca9
+size 2297
