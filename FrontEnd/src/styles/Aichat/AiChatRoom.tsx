1c23d434dc0b2d194f3c7e3951cf7d6a3bddb9c0
오기선 <pakox123@gmail.com>
2024-03-19 17:15:22 +0900

    feat: ai 채팅 stt 추가
    Former-commit-id: 377c267f767fe8473d26f5c212e2f20d4cd22a8e

--- FrontEnd/src/styles/Aichat/AiChatRoom.tsx
+++ FrontEnd/src/styles/Aichat/AiChatRoom.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc6f3f7e094b0d22e316c892337766681f3e21bec5f3eeb8c3fc056527234f46
-size 2911
+oid sha256:69c3f74f35dc029ce58a8b1b935f52b7e2b91d0184929fbff484e5ff7ec228e2
+size 3019
