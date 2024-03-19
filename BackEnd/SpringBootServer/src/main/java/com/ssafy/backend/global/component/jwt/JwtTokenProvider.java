ec9736d438a70895a1e6f194e6381cc643421cbc
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-19 12:17:29 +0900

    fix: 백엔드 서버에서 filter로 refreshToken 처리하지 않고 API 호출을 통해 액세스토큰 재발급 받기 기능 수정
    Former-commit-id: 689a9c8f323dfd4ecf43b83cba7b9252cf2bcb31

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/jwt/JwtTokenProvider.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/jwt/JwtTokenProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f4f75bfdb00d5cea1990bf75f882ff12577d2a4142ec57b3241f0c423a771f1
-size 8598
+oid sha256:4cfd02a6b3ad1e5d5cc76a05553c64d5b59d4397eafe4b7eacb7857756b02461
+size 6981
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/jwt/service/JwtTokenServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/jwt/service/JwtTokenServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:247d6254e171f6f2e0203fcf03468114adf84d8ca765323a82b51d21e0c8da9b
-size 2159
+oid sha256:ae5ed42fe4f08d6f30faaa757d79e5b713e98c0796439dbe77f4fd294a36a922
+size 2774
