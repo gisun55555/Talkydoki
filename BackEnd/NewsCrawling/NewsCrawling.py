6e55148b74ac043aa5b6b53583b577c22db4e903
송강산 <rkdtks0816@naver.com>
2024-03-19 08:54:36 +0000

    fead: 크롤링 도커에서 동작확인
    Former-commit-id: 9eabb9876781e9656c80a40e75edc8f7cb8187b5

--- BackEnd/NewsCrawling/NewsCrawling.py
+++ BackEnd/NewsCrawling/NewsCrawling.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f1583e4a4dfb3ed468eda0591e402724c020bf55f71d1d41220fee9360b755a
-size 6173
+oid sha256:fbbb291d11696420da47cab90206d87cdea735a2e3246e5310b071516495543a
+size 6180
