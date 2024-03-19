c8d4421e80db23c01adddaa1a5ace49a91314930
오기선 <pakox123@gmail.com>
2024-03-19 09:04:20 +0900

    feat: ai 채팅룸 페이지 작성
    Former-commit-id: 21b27bd7956e62f4601c7ae35430ed9a429a972a

---
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatRoom.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:384353eb2412dd79b98946b6fa9a520be4248b21e290f39e45187e47ed1b83ec
+size 406
--- FrontEnd/src/routes/AiChatRoom.tsx
+++ FrontEnd/src/routes/AiChatRoom.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f964d02747b1dc899479140485117f7996d46fdbfbdb47b80d944e4e8d5a7ca
-size 209
+oid sha256:c5ca7305579668b9affaf3b8ca89d0eb7ca821bfac72b8cb209219bfda00263f
+size 367
