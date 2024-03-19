e4ace2fedee9e2ed4e2d9f1609147eb7be421985
이지호 <jeho1129@naver.com>
2024-03-19 12:10:27 +0900

    feat : SpringBoot에서 사용자 단어 추천 API 구현
    Former-commit-id: 65dabe25d2bbdf289ab141fb4c4a9c0c62e0b55b

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/KeywordService.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/KeywordService.java
@@ -1,13 +1,3 @@
-package com.ssafy.backend.domain.news.service;
-
-import com.ssafy.backend.domain.news.dto.KeywordPostRequest;
-
-public interface KeywordService {
-
-    /**
-     * 일본어 키워드를 저장합니다.
-     *
-     * @param keywordPostRequest 키워드 데이터
-     */
-    void insertKeyword(KeywordPostRequest keywordPostRequest);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:35a25bc6a6cf3da2135a8ad02427be77f8d26a1521f7a6e9b48581820ddf3991
+size 574
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/NewsService.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/NewsService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3843e5e9cf69fffde0321f7787b9956f7522b4db933578a9080ee499c8e74ea9
-size 1169
+oid sha256:9483a5cf7a4e226764bb6cff43a7d4b452b70f3c03db99726c54b6495adc7400
+size 1166
