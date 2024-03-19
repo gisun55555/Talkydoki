76ebdd5cdc3dad63c67a9123395ece08836e29d3
오기선 <pakox123@gmail.com>
2024-03-19 17:15:22 +0900

    feat: ai 채팅 stt 추가
    Former-commit-id: edb9ad8443f2c9e5391b42cd2535eec86ab33cfd

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec9d4e5bea55e72f5b870b0330efab9bee4e64adfb51dbb765b5a083db668b34
-size 2903
+oid sha256:d6c2a9a5489123c3ab8e387ca86ea4cbff969551e5a37230502d51653367721f
+size 2941
