2e9751bc5950aabb9acf50003ca749cd9869535d
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-19 11:28:49 +0900

    feat: refreshToken을 이용하여 accessToken 재발급 기능 개발
    Former-commit-id: 5e0e8fef41585c5f20c799a13fc0133bc55066f2

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/jwt/service/JwtTokenServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/jwt/service/JwtTokenServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4850b9afda9c97787785fafe1dfca02f1055552ed0680251dd2e07ce08359e8
-size 1959
+oid sha256:247d6254e171f6f2e0203fcf03468114adf84d8ca765323a82b51d21e0c8da9b
+size 2159
