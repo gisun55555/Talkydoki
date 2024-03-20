1fa30affb24ccf738d3fa3e06c0c3d1a1f5a9c4e
이지호 <jeho1129@naver.com>
2024-03-20 15:01:56 +0900

    fix: EC2 내 Hadoop 실행 오류 원인 찾기 위한 디버깅 코드 추가
    Former-commit-id: f503b38a8e75ff89d04a6538323e93c8211b78a5

--- BackEnd/FastApiServer/data_processing/news.py
+++ BackEnd/FastApiServer/data_processing/news.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4186d0b8e6dfe06dd5522f99d8237eda1692d495c0ad9e2ba0837d62a4060380
-size 5352
+oid sha256:8f9582ce9b5fc35ac9ec282a7cc658de955239cc6d0609bb272f6f77b838d2d9
+size 6259
