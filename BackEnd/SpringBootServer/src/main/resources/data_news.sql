1441b7a7d74867b0975c1461e03ef13c16799beb
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-20 14:26:46 +0900

    fix: 뉴스 더미 데이터 최신순이 ID값 높게끔 수정
    Former-commit-id: ffa9f1dfb4cd66366c435ad94dd7f3d08192210d

--- BackEnd/SpringBootServer/src/main/resources/data_news.sql
+++ BackEnd/SpringBootServer/src/main/resources/data_news.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a7d88366e39a6e7499edd7b567c6f0b61748f063ca8b38fd4186bb15b8b11a9
-size 2224125
+oid sha256:9e36069817b4dc7b8b382de6b59fe652f4e97c8c09725f1285a82f655c1eb28e
+size 2223094
