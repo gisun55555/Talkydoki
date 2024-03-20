86a358113f01eb4a1f003a3a87986da220a898ca
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-20 17:50:47 +0900

    refactor: OpenAI API 관련 dto 리팩토링 진행중
    Former-commit-id: f9c70c29ffa39422331a4005bb06904a572b9167

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/dto/GptChatRequest.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/dto/GptChatRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3c80e0e354afc9526943edf2f9844cbca19bc475787cd880eb096c1e0af545b
-size 927
+oid sha256:86c67067acbb03b97aa8328a668ea041f1f0c88bcae37d3ee2a9627dc9c81ab5
+size 914
