24c9195bf28b91ef4ec6ee71043db8df16f9ec6b
이지호 <jeho1129@naver.com>
2024-03-20 23:06:04 +0900

    fix: EC2 내 Hadoop 실행 관련 Dockerfile 설정 추가 11
    Former-commit-id: 95539eb8d022798627a66fcc7af8a3804d704318

--- BackEnd/FastApiServer/data_processing/news.py
+++ BackEnd/FastApiServer/data_processing/news.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce2a0156b12c6af7b62d7b314c2143cd0251f08268c491a5c2f8b91916546076
-size 6290
+oid sha256:50571871749be4dabde2c915b53caac90b518e635f4e7034cb2ff037583cd447
+size 7293
