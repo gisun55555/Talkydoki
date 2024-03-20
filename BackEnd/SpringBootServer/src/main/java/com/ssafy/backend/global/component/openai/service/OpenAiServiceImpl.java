1bfb68107427da1d59df9ca69073d3018782690a
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-20 17:50:47 +0900

    refactor: OpenAI API 관련 dto 리팩토링 진행중
    Former-commit-id: 98c703f60431a7b5777e61d63eb0f5e6365562b7

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/service/OpenAiServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/service/OpenAiServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2de7b698dc913305fd22566747c608cd183143bee03fce5dd52b6665b3f8c40
-size 3348
+oid sha256:30a975f5e79eea705dbb7364ede69361a205b0fdcb3743a2f33265a422bdee03
+size 3349
