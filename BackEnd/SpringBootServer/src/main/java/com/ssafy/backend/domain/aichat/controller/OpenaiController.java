a0ebcae8bd6019f9fdf08c6eb820a1beb08e3fad
조현호 <chh5680@naver.com>
2024-03-20 14:35:06 +0900

    refactor : Controller에 swagger 표시
    Former-commit-id: 271eab1897d7d20a57b3ddf4d0e98cc3d29cfc98

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/OpenaiController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/OpenaiController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03c1e20a5d7bbe509730e51a0b6cfbff67210bb6346b710d50326a8da503e595
-size 2444
+oid sha256:c7d3024188383bb2da070ba254b23310f12e4762c5b5295c184dfcc250b52862
+size 2723
