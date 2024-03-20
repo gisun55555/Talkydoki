a6d7c472c7b9fd9f27bcb92d6428fb42500d88b1
이지호 <jeho1129@naver.com>
2024-03-20 14:09:33 +0900

    fix: EC2 내 Hadoop 실행 로직 수정 5
    Former-commit-id: 4a26021ed1a82657d1d8a117a211ba07206a2cda

--- BackEnd/FastApiServer/data_processing/news.py
+++ BackEnd/FastApiServer/data_processing/news.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b802e91ca86821b18893b70efbeb1eac80385a2a34d3358f009b63e0881797d7
-size 5250
+oid sha256:ccae1823e009f70a961d5f8bdec47732b14c5ba38cfb9d9664928a93ba419c37
+size 5119
