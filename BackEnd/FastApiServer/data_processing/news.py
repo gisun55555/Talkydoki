d3769e54fef1f43741c6cae6adf03f6e5f36b967
이지호 <jeho1129@naver.com>
2024-03-20 16:00:23 +0900

    fix: EC2 내 Hadoop 실행 오류 원인 찾기 위한 디버깅 코드 추가 + Dockerfile 설정
    Former-commit-id: 40e70c112864d709d791060a755060728efd3152

--- BackEnd/FastApiServer/data_processing/news.py
+++ BackEnd/FastApiServer/data_processing/news.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8e678a8ec8c7e32ee907cafc8c5da26a52b800d04a597767c7bec0868159c2d
-size 6026
+oid sha256:aa91475e13345509856db0bb1126673bbedb121a9fc10625380287f07651b48b
+size 6042
