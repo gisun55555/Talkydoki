e38b8b611b5c2cae6becd3113fa83d4c3329b81d
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-21 13:57:17 +0900

    fix: OPENAI api key값 인코딩
    Former-commit-id: 868ab7181796d5e8fabbf4fbe5557faa5ee97798

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:067d4aec45e824a938b5aa4904e48514de8af6cedcb3bcb68253cee41a5e2ef7
-size 2423
+oid sha256:98c9f3d9fb084795928bcabf69d6780434de4e4d1228d9d20f14a693b296e37e
+size 2493
