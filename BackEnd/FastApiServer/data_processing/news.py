fee9213391f7b7d18ce2354af8f82825373b5872
이지호 <jeho1129@naver.com>
2024-03-21 11:59:59 +0900

    fix: 뉴스 - 단어 데이터 처리 로직 통합 Refactoring FINAL
    Former-commit-id: 4d3496b1526aed96360817593b54dad8c9eaa472

--- BackEnd/FastApiServer/data_processing/news.py
+++ BackEnd/FastApiServer/data_processing/news.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83207d30fb09530f48c8800070d2376f59d5f783a9489cdf13fbf6ee77b0a707
-size 7646
+oid sha256:dc78f4eeeea0e602f4bf745d0067bd035d1300775c5ed2fc89cb3ce9625ad22e
+size 7805
