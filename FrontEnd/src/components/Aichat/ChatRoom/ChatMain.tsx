aacfd6ba8fd1a46c136e694b1b6fc8fcde340f2d
오기선 <pakox123@gmail.com>
2024-03-22 08:55:47 +0900

    fix: 채팅메시지 컴보넌트 분리
    Former-commit-id: c3e47a5c07a0e8d7fb7b8a188ca4094d808e58df

--- FrontEnd/src/components/Aichat/ChatRoom/ChatMain.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:815bc4284543e19e6b26994c3af2dd1824d14947f05c1e5003641b9f48e243ff
-size 5561
+oid sha256:49c32dc367d509a29e88e25c4794eaf8a7d89b028c3de0fb9783ba4472b5ac14
+size 2274
---
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatMessage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4fc3a2dae667284a1dab852d2aeb3215465e791845a75bfd2e4021b1dfc7bbf
+size 2589
