80a53ba25685d0fa3e0684ec2497bf7ab5bf3932
백민정 <peku9693@gmail.com>
2024-03-21 09:14:37 +0900

    fix: 빌드 오류 수정
    Former-commit-id: 8d5a1192de84d690229b0cfce098b175445d34ac

--- FrontEnd/src/components/Aichat/ChatRoom/ChatMain.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:628daad6099f38f5d00065a22d16be68fabf19d7ed0bea88bab36b965ff7d405
-size 5494
+oid sha256:815bc4284543e19e6b26994c3af2dd1824d14947f05c1e5003641b9f48e243ff
+size 5561
