84865d03d2c16d0d2ea3b5ea12cdd11be797c3ad
송강산 <rkdtks0816@naver.com>
2024-03-21 10:20:49 +0900

    feat: 뉴스 크롤링 이미지 저장 추가
    Former-commit-id: a9bdc3c9c046d72c1bebf0ef8632abefed763c24

---
+++ BackEnd/NewsCrawling/Url-dev.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:888e158579fc0dcc462859156ac1583b493cd302e9c921864fc52419235ff263
+size 56
---
+++ BackEnd/NewsCrawling/Url.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f5f777857c521109ebacaec9dcdef369a2a5963048b5511d8fb4bf848e52b730
+size 51
