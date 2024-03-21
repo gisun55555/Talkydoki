0c6e22123fcc5f3bc5f850510eb921a46a63c7c4
이지호 <jeho1129@naver.com>
2024-03-21 10:14:44 +0900

    feat : 뉴스 - 단어 가중치 DB 저장 로직 추가
    Former-commit-id: f61717524933c1b4712aea553cd76f2e62b8ec39

--- BackEnd/FastApiServer/data_processing/news.py
+++ BackEnd/FastApiServer/data_processing/news.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef7fa68f4cb3144a00b4efa65a89ee843db5c47a84602b05b1c0784f6476e915
-size 7319
+oid sha256:d325ec538c2a086183560e0eadd3fc0c6d8c9ffe4fe158dee3caa86a4fd19fa0
+size 8134
