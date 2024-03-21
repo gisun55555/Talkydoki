4a9e3c4d9d5ffc6dcb966def1c7b4d23f289a8c8
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-21 15:53:15 +0900

    fix: AI 회화 채팅부분 사용자 입장 메시지 보내기 @AuthenticationPrincipal 어노테이션 이용하여 수정
    Former-commit-id: 49017c589d2602c306f77495100e046654da4690

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98c9f3d9fb084795928bcabf69d6780434de4e4d1228d9d20f14a693b296e37e
-size 2493
+oid sha256:bbc8fba7790e31ab00921976c17dacb29c6d7e859b0d4b4f13b79bc4a8c34491
+size 2618
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc99f80d0588e21904e5540fad209d1c9c4e1431a72c448dfc834c47c565ada7
-size 5259
+oid sha256:8a91626550a47acbf4e9d8282abf6380bcedc9c82d3e03f326026a4318dde197
+size 5257
