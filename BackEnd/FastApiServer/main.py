377b1de569b9acd193beca70d1ce2c339753cb89
이지호 <jeho1129@naver.com>
2024-03-22 13:14:38 +0900

    fix: 사용자 더미 데이터 DB 저장 SQL 파일 추가
    Former-commit-id: 687272b094f803ab01838020c55b7fccca68c6f3

--- BackEnd/FastApiServer/main.py
+++ BackEnd/FastApiServer/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a09faab8b4f41516dd80b21a0eba0dbc77a4d9a7047a9636aed415bb99d3e12
-size 2117
+oid sha256:7219af60add7d7d24ffc4896d7559d5bc31d2fbeacd0b683e347d7f5dcc406fb
+size 2150
