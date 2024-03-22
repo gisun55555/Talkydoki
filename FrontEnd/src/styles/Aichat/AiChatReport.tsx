8285a14309dff1a2b72a8dd880af725470700372
송강산 <rkdtks0816@naver.com>
2024-03-22 17:52:03 +0900

    feat: 채팅 리포트 틀, 차트 개발
    Former-commit-id: 5d5dc09ff399abcaf010c9155b2422424e365ae3

--- FrontEnd/src/styles/Aichat/AiChatReport.tsx
+++ FrontEnd/src/styles/Aichat/AiChatReport.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8fdbe051fb8b6f1f37a2888983aa6914d5ae1f67a99cd23801ea1b91603ed0f
-size 183
+oid sha256:0a6d57487b2f6b3972617748d23d1c1dd50068254d1e9f7b25f75e62d1043f3d
+size 613
