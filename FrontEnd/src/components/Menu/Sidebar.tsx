e7e8e0862ab96f07e353c81e54d09f0e910fcb4f
백민정 <peku9693@gmail.com>
2024-03-22 16:46:13 +0900

    fix: 빌드오류수정
    Former-commit-id: fa894e8fee87b96b935359e2c2f95dc3cd82bf07

--- FrontEnd/src/components/Menu/Sidebar.tsx
+++ FrontEnd/src/components/Menu/Sidebar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0665ffbce7709c73d0dafb5e107c6c95abcb050c753d9d4d6712195df1baf97c
-size 1506
+oid sha256:0c1a80c311ae59d3031e5ec9de2e5160ebc0e0298b55143a373f969e293cb1c1
+size 1486
--- FrontEnd/src/styles/News/Detail/container.tsx
+++ FrontEnd/src/styles/News/Detail/container.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:586e9cc00b0b69cb9867c793ae5a4b41b7f69399b31d9bfcb5cfbfa78e819508
-size 1710
+oid sha256:527aeb4d6da9eabbbd7b7d2509741ff559608ade51623644b638a7cae01ad1b9
+size 1677
