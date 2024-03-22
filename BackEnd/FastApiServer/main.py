9d0537d10fae6e65206fbe3976764f25d11c89cf
이지호 <jeho1129@naver.com>
2024-03-22 12:02:04 +0900

    fix: 사용자 더미 데이터 DB 저장 로직(requirements.txt 추가)
    Former-commit-id: 608c14beb3d2f89b1a7099eedac094a844f9df40

--- BackEnd/FastApiServer/main.py
+++ BackEnd/FastApiServer/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed632f391d1429b99611654ff8e1e0009d0026d9882a01694051f4e7aaaabe8f
-size 2110
+oid sha256:99c8ed9b07cf433a3c4a2a8abeadc02ff9748deabedeebb94b06de9ed0f7ab3e
+size 2111
