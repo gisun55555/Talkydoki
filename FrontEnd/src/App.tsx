b2efc9e32893b117a53f544f30927fea35c5f93b
오기선 <pakox123@gmail.com>
2024-03-22 15:37:00 +0900

    feat: ai 채팅 레포트 페이지 추가
    Former-commit-id: 521a82a1f77cac1dd31ffa992e5dcfdc62ae6d03

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99b20e1c6e31a20b19455bbde62753cc859a045a0eb1a5890fd8ccec83626989
-size 2904
+oid sha256:4af118c9afe229923e542ceeae3f3ef2251e637f187b95fa2543ce9079768211
+size 3026
---
+++ FrontEnd/src/routes/AiChatReport.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:107992d57f1453b8025e45747d16e219721b6a2af22c716d4037ab5e9a7b1e1c
+size 341
