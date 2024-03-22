79699816b56f6d027265972767c8a40f4a799828
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-22 17:32:14 +0900

    feat: openai api thread 생성 기능 개발
    Former-commit-id: 50d3522d36f65f88f0a34a58eff8d3614fe6dc0f

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/entity/enums/AiChatCategory.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/entity/enums/AiChatCategory.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1504ccaa8464bffed670ca3950ad91f617a9d047f4db91170d30d279f20890e4
-size 556
+oid sha256:52c015142af5d6492dfc4b90837cfad0ca70d806bcf351947baf0e1a86a60211
+size 650
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cacbd3e85e17232428c9a18309c36dbee214f6e68db13001f3789eaa42caea1
-size 8195
+oid sha256:2b51d5940af7299a8bf3444a8baab2cdbb318d57b4e6ef7d1505ee628ef0a27c
+size 8411
