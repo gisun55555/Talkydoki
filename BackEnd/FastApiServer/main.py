a7e50d12a355c4d779951577eed7c2e84506807c
이지호 <jeho1129@naver.com>
2024-03-22 11:28:28 +0900

    feat: FastAPI 실행 시 사용자 1000명 더미 데이터 생성하여 DB 저장 로직 추가
    Former-commit-id: 205e42097f4834f4050471227edab77bcbcb6d9a

--- BackEnd/FastApiServer/main.py
+++ BackEnd/FastApiServer/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4128a9c4047dbfda1ba699dfa71ce97c1ad37defdeb7d86877773b5ea4a0cc63
-size 1028
+oid sha256:21434f92614c2b4af5520d441be00fc835590bc0dbf6bd2b82af5674acd42927
+size 1799
---
+++ BackEnd/FastApiServer/models/member.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:27a47899047cddb36d19722678f5138170501f41e0bb54ad7d8fb1e09c801ede
+size 632
