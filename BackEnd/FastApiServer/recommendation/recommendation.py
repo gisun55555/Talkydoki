845e5989e22167a69e81da32ef8e78611d9673bf
이지호 <jeho1129@naver.com>
2024-03-25 02:38:45 +0900

    fix: 실제 DB에 맞게 뉴스 + 단어 추천 알고리즘 / API 구현 완료
    Former-commit-id: 52a9cdb74d414863b2c8ca206247c53d4206f998

--- BackEnd/FastApiServer/recommendation/recommendation.py
+++ BackEnd/FastApiServer/recommendation/recommendation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69c61853d8e81f87cd50f51bc38176ad795b2a84a98497e6ed932a9ee0b8cbde
-size 10378
+oid sha256:15ba62c77d31c252bb3f320fb555f886b15fd4bd623c26d653a90ba1c31a8d09
+size 11110
