b6b6aec4e7634be4f59829d4dda0768d69b58bf3
오기선 <pakox123@gmail.com>
2024-03-25 12:07:45 +0900

    feat: 이메일 인증 메시지 추가
    Former-commit-id: 1ba60809412be017429e0bf7e83f84377a7e791e

--- FrontEnd/src/routes/SignUp.tsx
+++ FrontEnd/src/routes/SignUp.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d6cb821a9c5dd6ed0cd72f6659e4099db80b2d26ec0bdbd1bce1936b9687622
-size 5088
+oid sha256:5badd455cc3602a3e552091daa579e7db306f8c45f3cf842db5be07224b3795c
+size 5403
