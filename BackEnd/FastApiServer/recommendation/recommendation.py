e5bb817a772cfc4e3495cd18d1855c0daa62d1d5
이지호 <jeho1129@naver.com>
2024-03-25 14:56:32 +0900

    fix: 뉴스 추천 기준(현재 시간 기준 5일 내 뉴스들) 변경
    Former-commit-id: 0fb279c330be3cd7c689cb23617afa79f1f61114

--- BackEnd/FastApiServer/recommendation/recommendation.py
+++ BackEnd/FastApiServer/recommendation/recommendation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d5759576d4645b5f5a88d59dad8d946d4edc44944d906903f4386b23383e4f1
-size 15014
+oid sha256:a3094cf69818bf4f20ce93d443d5acbadd28d7fc632f7afec0c65c92d726a3b4
+size 14857
