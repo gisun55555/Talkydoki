ad5cebe0639aaccb5cf336dfa1243fa02b6b9aa3
이지호 <jeho1129@naver.com>
2024-03-25 13:17:39 +0900

    fix: 추천 뉴스 반환 시 뉴스 해당 정보 반환하도록 수정
    Former-commit-id: 3fb36a15222ad4adc571e050d063e8d2b4a47c5c

--- BackEnd/FastApiServer/models/news.py
+++ BackEnd/FastApiServer/models/news.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47b32bd67a31f0904243846ecbcb56f0c870f0ad996f3ea2d294a07fc1c6082b
-size 574
+oid sha256:f896e7b2568dfaa316d0a552683f8ef5643787f14f0ced3046a6e68c763253fe
+size 641
---
+++ BackEnd/FastApiServer/models/news_image.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:72a3090afb25c5915578a38968eafc2ccbd6a2e7228137b81b33494fa9c809fc
+size 436
