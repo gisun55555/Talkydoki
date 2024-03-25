918d142965b15b99956acf8943cacd7e2a24fbbd
이지호 <jeho1129@naver.com>
2024-03-25 12:06:47 +0900

    fix: 가중치 데이터가 존재할 경우 업데이트하도록 수정
    Former-commit-id: cee0cdc0fc25f4d5a00d2c3f2f37183e96528643

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/entity/NewsKeywordMapping.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/entity/NewsKeywordMapping.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:828d875a571a9e0fec1bf4b7dd2a28a1a303b671250ecc582f8de91f7a30aeb0
-size 670
+oid sha256:f9eb83c2a8aeefc8e6f8ad09e5627c4724e09f4e034d8e8d0e8d6d7197f05dc8
+size 678
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/KeywordServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/KeywordServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d7d68fb9db047044e35fe77ce343221b33ab5fd4378f94c546457e3f554346c
-size 3022
+oid sha256:8efc977570e9066801927d4d8c75eed3c658eb1d0df051858e8f07ac35d18442
+size 3451
