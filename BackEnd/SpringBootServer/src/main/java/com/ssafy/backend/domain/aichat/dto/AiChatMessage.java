d184ee6ee51397fe140fc2f819943c9e324397e8
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-25 12:18:42 +0900

    feat: GPT 응답 내역 redis에 저장 로직 구현
    Former-commit-id: de8247b58129aaf5b752efd526d64dc78e8bd6ef

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/dto/AiChatMessage.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/dto/AiChatMessage.java
@@ -1,11 +1,3 @@
-package com.ssafy.backend.domain.aichat.dto;
-
-import com.ssafy.backend.domain.aichat.entity.enums.AiChatSender;
-import com.ssafy.backend.domain.aichat.service.AiChatServiceImpl;
-import lombok.Builder;
-
-public record AiChatMessage(
-    AiChatSender sender,
-    String content
-) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:65a7ef3acf87b4cc3bfb1f7d02e7acc8a6696be3b29e9cffe423513fabcc281a
+size 290
