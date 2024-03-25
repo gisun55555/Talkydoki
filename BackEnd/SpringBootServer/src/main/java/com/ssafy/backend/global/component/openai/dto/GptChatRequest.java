d61b7fcaa5caf03061f3a851a2b610ccc6c8fc53
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-25 15:05:06 +0900

    feat: GPT와 일본어 회화 주고받기 기능 개발
    Former-commit-id: 0f2cd426f8a7e0410fb4680bbf4047d062bd6c51

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/dto/GptChatRequest.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/dto/GptChatRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f120e84a45ac75eab91ae42b976034b85179db9c322676fa5aff042e3249f65
+oid sha256:f2d9cf0d8474b83eb8d0bd5709170a9a7572352dabec4377808fa17ada1edf2f
 size 1302
