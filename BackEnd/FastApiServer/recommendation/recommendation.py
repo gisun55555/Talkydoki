cfaa52169bca6bcb22928f3a148cc3f9c28e0a2e
이지호 <jeho1129@naver.com>
2024-03-25 13:17:39 +0900

    fix: 추천 뉴스 반환 시 뉴스 해당 정보 반환하도록 수정
    Former-commit-id: 3792a6371aa78197e6f5a696c7c329a3177720e9

--- BackEnd/FastApiServer/recommendation/recommendation.py
+++ BackEnd/FastApiServer/recommendation/recommendation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d0ec0fbb5aa6743349f0ac1dbc87e1d8eb516ae53107e039e9810d6081d02b5
-size 13774
+oid sha256:3655739f61676341accaf3eded7e500623f6be07954a2a1bd3e954f86a62ca2f
+size 14630
