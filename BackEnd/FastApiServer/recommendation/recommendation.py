8204ef249ed466efd04533142e4dec7630da7a0e
이지호 <jeho1129@naver.com>
2024-03-25 12:06:47 +0900

    fix: 가중치 데이터가 존재할 경우 업데이트하도록 수정
    Former-commit-id: 9361284d5ecd26d309f22fb9b2575f3b73335c46

--- BackEnd/FastApiServer/recommendation/recommendation.py
+++ BackEnd/FastApiServer/recommendation/recommendation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cb0089768cb452ae62d61e150d5ef739b4beae5af1ced3b03cd72f23f9d7386
-size 13811
+oid sha256:1d0ec0fbb5aa6743349f0ac1dbc87e1d8eb516ae53107e039e9810d6081d02b5
+size 13774
