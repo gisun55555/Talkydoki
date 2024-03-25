b12a1caae727ff762f128f10babcf46b53906083
이지호 <jeho1129@naver.com>
2024-03-25 14:28:51 +0900

    fix: 뉴스 추천 API 리팩토링
    Former-commit-id: b1e9ad25f2652255319ad50e15e8c5265ddd7503

--- BackEnd/FastApiServer/models/news.py
+++ BackEnd/FastApiServer/models/news.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f896e7b2568dfaa316d0a552683f8ef5643787f14f0ced3046a6e68c763253fe
-size 641
+oid sha256:22ea97fb756487749d69b49b6d73fbb863c8024d6144a93ea93b21ba64884fb3
+size 875
--- BackEnd/FastApiServer/recommendation/recommendation.py
+++ BackEnd/FastApiServer/recommendation/recommendation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58a051760167c3a13a52ef4ddda297695ef31bb698b7a64a4a599bbf1ece3e12
-size 14670
+oid sha256:8fbee75d7a50e93677e3aefad12312d93d2be32b4435fe988314126df852101a
+size 15016
