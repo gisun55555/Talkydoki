1d4ba60dd4e538b5da52e65df91a577a1d427586
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-25 12:18:42 +0900

    feat: GPT 응답 내역 redis에 저장 로직 구현
    Former-commit-id: 69dd025d9a6b1bf817c2c3d3351efa004287540c

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90ad74852846323c0ac133b1db34180158285295f03edea2c5d7a880cefa26ff
-size 9744
+oid sha256:4a8d40a4579a94dcf16bb15d4cbdddfb71d0abc0ff137344c14b551678f46b22
+size 10569
---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/repository/OpenAiSetupRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:200e64c402e6dbb4d2aa19efd838015ec49f8eb2e07ac8b93234dc0297a2d906
+size 1878
