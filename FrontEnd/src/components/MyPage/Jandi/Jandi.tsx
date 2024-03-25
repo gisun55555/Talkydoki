68ab797f1f41c634ed1a6618764107606e1d5b31
오기선 <pakox123@gmail.com>
2024-03-25 17:44:22 +0900

    feat: 마이페이지 메인작성
    Former-commit-id: 374dc9491c993e2ae967fe34008c3a985488d70e

---
+++ FrontEnd/src/components/MyPage/Jandi/Jandi.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a2db689ea7569c9d80d36081cc7050b945bd6588050c839619e6b30b8af35779
+size 964
--- FrontEnd/src/routes/MyPage.tsx
+++ FrontEnd/src/routes/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d6e226e3917e52ecb015e071fbf55ed5bfdd6e79df77aa88db5ff0258ad1969
-size 995
+oid sha256:3c0be85ddd6ef9110dc0a836c1917cfdeb9781e489dba2502cad643297300e14
+size 1253
