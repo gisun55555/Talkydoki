77c271e67a73dc3445122c34a7471edd730a40d3
이지호 <jeho1129@naver.com>
2024-03-25 17:35:05 +0900

    fix: 사용자 더미 데이터 1000개 -> 600개로 줄이기
    Former-commit-id: 2b68bd9d170dbe1989eb082605100fe16c7cc536

--- BackEnd/SpringBootServer/src/main/resources/data_member.sql
+++ BackEnd/SpringBootServer/src/main/resources/data_member.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dab7885459ad37b462df07a2e78f3b9434a12565d797216a3e1239d66a045e73
-size 146509
+oid sha256:64d3cffc928bd54157845189f4b2d10ef04e4be6ba23267f4c5a29dbaf9093cc
+size 147727
