afa27613b4a2e857af7fffd6c1d4b517472728c4
오기선 <pakox123@gmail.com>
2024-03-25 17:47:59 +0900

    fix: 로컬ci 수정
    Former-commit-id: 03eccd750650d66ab83d9b8a5977cd45baa3d217

--- FrontEnd/src/components/MyPage/Header/InfoDisplay.tsx
+++ FrontEnd/src/components/MyPage/Header/InfoDisplay.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58371f9e5db13eb4af6bf5b628d52fec85219784a1869e77cd0049f856b0f8cb
-size 304
+oid sha256:419ad518725d0e6a3b9a0703164c636f90aeae06a11156aa03a03e5407f3b712
+size 276
