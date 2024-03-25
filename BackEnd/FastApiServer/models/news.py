1d04787ec153ade424f6ab706a8589771ff663bb
이지호 <jeho1129@naver.com>
2024-03-25 11:14:13 +0900

    fix: 3일 내 기사에 대해서만 추천하도록 수정
    Former-commit-id: f47acfc283b1d862ae853749169e6b6db106a92e

--- BackEnd/FastApiServer/models/news.py
+++ BackEnd/FastApiServer/models/news.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a3db722382a354bcee8a80cd38873bb0844f8fd78fdd0c37b42c4da264557ae
-size 514
+oid sha256:47b32bd67a31f0904243846ecbcb56f0c870f0ad996f3ea2d294a07fc1c6082b
+size 574
