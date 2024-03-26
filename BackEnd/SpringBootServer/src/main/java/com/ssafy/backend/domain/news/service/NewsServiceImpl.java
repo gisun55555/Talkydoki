93da60ffc682b6b808826d6d8318ffa28eb1705b
이지호 <jeho1129@naver.com>
2024-03-26 10:58:15 +0900

    feat: Shadowing 평가 값 DB 저장 로직 구현
    Former-commit-id: 422d0985563d4fea04e4958273751815a3742ba5

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/NewsServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/NewsServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54fc34392cc828fc68f9a5df059128c5765638e81bdcbe87db1b76428deea035
-size 6721
+oid sha256:779fc8e03c0f4e8da65ccdfe39ad03eaf5c99c20bec02802a0d471584928a221
+size 6895
