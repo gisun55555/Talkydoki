a9c6c9424588ce26f1ed178afd4b7bc92b43c67f
이지호 <jeho1129@naver.com>
2024-03-26 12:01:00 +0900

    fix: News 예외 처리 수정
    Former-commit-id: f7e7526e363d2c591b2406a131bbf8a7d52ed680

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/NewsServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/NewsServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90e60c0f24a7c894201e06781b8103dcc52e71d4cbc7e808f3f9668eeb3b2bc5
-size 7407
+oid sha256:9a08c3f85507c4c45d4ac2749dc4ada99d8c3e70f3681cfaa0b92c9b59a50492
+size 7398
