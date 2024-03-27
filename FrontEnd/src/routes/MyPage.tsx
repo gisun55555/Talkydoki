a3c571ca8f86db792ba20ad7cb34b608f0abd45b
오기선 <pakox123@gmail.com>
2024-03-28 00:36:07 +0900

    feat: 마이페이지 수정
    Former-commit-id: 2724103d07b3b10ecfc9c8f13666773cf8a9374a

--- FrontEnd/src/routes/MyPage.tsx
+++ FrontEnd/src/routes/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d77e9db680525caca3d7aa086948ac8c78d4ec1007cb72dad770490e6937bf19
-size 1256
+oid sha256:3cb8b080d590a1c88e4fa4a2cc56bcbeb10917d5abec644b4b59b92f0f53f6e7
+size 1538
