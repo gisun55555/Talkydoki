8209856a9b95d04290f9290165414cab2bb256a3
조현호 <chh5680@naver.com>
2024-03-27 16:58:21 +0900

    feat: /gpt/{roomId}/report api에 대한 swagger 코드 작성
    Former-commit-id: 828878252b9ce926e1b83b21b27ae4adf1ed025c

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10248e8a5c02a86b659bbf53f4bd076e581e180cfd9c4b3035fae37b13913c03
-size 4071
+oid sha256:d07fd64c42d14306df7f698ffc2ce458de0867fd202b7b060d42b3932252ddab
+size 4340
