d02bf7cd2af90a9253043538c36398ea2dfa4f84
BMJ <peku9693@gmail.com>
2024-03-28 01:08:14 +0900

    fix: intro
    Former-commit-id: abaf5030e1347f6174ffd08b54e3619ac46ec78c

---
+++ FrontEnd/src/components/Intro/contents/PageSection.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:527a797491de42440ea11d55ecd247d2d14cd4c26bea8fdea063c86559ad099a
+size 2100
--- FrontEnd/src/routes/Intro.tsx
+++ FrontEnd/src/routes/Intro.tsx
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f919c252ce87f996184398d0bdda7fe816b052c3f34825a3c3ccfa4040f78176
-size 4045
+import React from "react";
+import { IntroWrapper } from "@/styles/Intro/containers";
+import IntroSidebar from "@/components/Intro/IntroSidebar";
+import IntroContents from "@/components/Intro/IntroContents";
+import IntroBackground from "@/components/Intro/IntroBackground";
+
+type Props = {};
+
+function Intro({}: Props) {
+  return (
+    <IntroWrapper>
+      <IntroSidebar />
+      <IntroContents />
+      <IntroBackground />
+    </IntroWrapper>
+  );
+}
+
+export default Intro;
