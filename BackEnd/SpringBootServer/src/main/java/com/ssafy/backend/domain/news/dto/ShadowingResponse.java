cfef217f7e84e434475a7cae16f42d5004edc33d
이지호 <jeho1129@naver.com>
2024-03-27 10:54:54 +0900

    fix: Shadowing 점수 반환 수정(0.5점 단위로 출력되도록)
    Former-commit-id: a3a98090e26e0902c1bfc66c79bd6985017ba031

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/dto/ShadowingResponse.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/dto/ShadowingResponse.java
@@ -1,4 +1,3 @@
-package com.ssafy.backend.domain.news.dto;
-
-public record ShadowingResponse(int similarity) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c35e5be0593457b00f983c8a82b4bb0404a766110fab161c861112228065a87b
+size 97
