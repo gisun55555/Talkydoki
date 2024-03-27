6e3503b38b7abfd6243ade35bcb75e932e0a23d8
조현호 <chh5680@naver.com>
2024-03-27 15:42:05 +0900

    refactor: prompt 질 개선
    Former-commit-id: a60c92d24d68669cc7d9d7fed9fb215535426cec

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/dto/AiChatReportCreateApiRequest.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/dto/AiChatReportCreateApiRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d819fd452e2c1296b94daa06aaae7010c8f251e40ca0cb18b83588e40d688d8
-size 7275
+oid sha256:aeec309a8edc5ab4735db4fe124b9f9e185aa52136c46b7173a08d7dbd7276c2
+size 7384
