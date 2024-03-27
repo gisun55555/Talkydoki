c4e1c286c0ae62f4a395824f0202816a326db055
오기선 <pakox123@gmail.com>
2024-03-28 00:36:07 +0900

    feat: 마이페이지 수정
    Former-commit-id: e1973adf49243e64a5d9371b4d4ed1618c29f9c8

---
+++ FrontEnd/src/components/MyPage/MyVoca/MyVocaSimple.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8069f39dc42d1092f669252af58f701e28647fdbf7ed47097c7a4bc455bc4b74
+size 2200
---
+++ FrontEnd/src/components/MyPage/MyVoca/StartCover.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a7698be2d2a750ef6dbedf0ab1a07135aadb95aae456b9dcd9260646c1b553d
+size 818
