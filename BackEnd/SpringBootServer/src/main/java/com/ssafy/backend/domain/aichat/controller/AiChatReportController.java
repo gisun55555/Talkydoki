be64dcea2bc12aeab54ca2541be70c14210259d1
조현호 <chh5680@naver.com>
2024-03-27 14:14:36 +0900

    refactor: 레포트 디테일 조회 응답 DTO 속성 수정
    Former-commit-id: 330020a069c38a90efe518d123d24ad9eccf79bd

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatReportController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatReportController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6e9f097a48ea5b379186fae6af34f93676a9f993bfefd59bcf76d7db6264dae
-size 2436
+oid sha256:8f40ff9c1bae22b7502ca43ea88d81f24533cb774113a3921ab248fe79210cb6
+size 2435
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68e7ff83fd8c6d00f2384a90f6a5dc1f4a573ef0cc0d798b090309a189eb72e2
-size 14634
+oid sha256:6b3740465512bdd0931687e2397ad5923e03a217aeef1205bf7361fc6882dd75
+size 14759
