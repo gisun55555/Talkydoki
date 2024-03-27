bcf0211a9dc17ef1dbffbf8757a3681fdc655968
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-27 13:55:37 +0900

    fix: redis 캐싱 작업 수정
    Former-commit-id: 5b8a399cd5466c3549b96950af073d35828d3850

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbeef749c60b5ef71b7207678b4662c5345962e3f5c08c1a1d0b664fadc850ad
-size 12414
+oid sha256:f2b9e9ec22ed41ac0cf419f359e0ef70cf2157fedb5e492f43f9efeb241edbf5
+size 12156
