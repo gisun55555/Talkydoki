83fa6ad676f8e2473fac8bc8f77946c6433ebb52
이지호 <jeho1129@naver.com>
2024-03-27 19:07:36 +0900

    fix: 날짜 출력 형식 변경 2
    Former-commit-id: 12a0030c0aab555201010f14646d461bd8f070b0

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/service/MemberServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/member/service/MemberServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e472c45f7a06282d24335153be2fdb49d4bac5d65ed3cf34af1b07381441e5ff
+oid sha256:73f9b45d9451aeab6ad3af162b4e8c233bef2123b1ac7c6d16cace97fd50e8fb
 size 6333
