cc78f2161b1eb20e75c8d83d34741aacf1d1263e
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-27 13:48:42 +0900

    fix: 해당 채팅 메시지 메시지브로커에 전달 기능 수정
    Former-commit-id: fc44b3b7c567d915659a764ad62a1211eded46f7

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/config/RabbitMqConfig.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/config/RabbitMqConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09fdfab5e883c693c51dbdcedc64e77b2fb5030e26ce91a1aa86a3d41124390a
-size 3085
+oid sha256:79f48116fe1048a0e98d8b639ddc2d09c1f7c5c5c5d0b7656e2864245083f847
+size 4241
