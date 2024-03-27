e4313ff993c4d4ffa9be695d667cd97d779c0154
BMJ <peku9693@gmail.com>
2024-03-28 01:08:14 +0900

    fix: intro
    Former-commit-id: 49c961c2f58a712da34667fca600a2b7f46469a8

---
+++ FrontEnd/src/components/Intro/contents/IntroSection.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5368b2e8337928f089de96a6cb15030c6944a92351ae2d1bf43fd7405725b362
+size 1963
---
+++ FrontEnd/src/styles/Intro/containers.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c59f6be73713204542f1d2c562d366089b335ba7de0d677f1f927f3aa2f9535e
+size 3133
