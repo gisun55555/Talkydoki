00345220ffbea4c85d14ed11342626a3e36045d5
조현호 <chh5680@naver.com>
2024-03-27 13:38:48 +0900

    feat : 만들어진 레포트, 피드백 조회 기능, 유저별 레포트 조회 기능 구현
    Former-commit-id: 6a7171a8e7a4ad0df8a3e40c75b16bde90b46c62

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatService.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00a1a88d2f489275350ef989dbf9de9117170be06d2cc1d22aa2d6b116d45450
-size 2041
+oid sha256:8facfb3089ab6be204abff6e3d4dfae168283314e8b4af906e95854314d7fce5
+size 2233
