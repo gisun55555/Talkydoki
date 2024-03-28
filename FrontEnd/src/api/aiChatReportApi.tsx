0724f62c981805eb572648f32bd651c1663c8c9c
송강산 <rkdtks0816@naver.com>
2024-03-28 14:01:15 +0900

    feat: 채팅 리포트 API와 연결
    Former-commit-id: ba4f33602a151d53cf3fabd732c9104ca3d3a978

---
+++ FrontEnd/src/api/aiChatReportApi.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:494fa7a2edc1dd99483edc81e236d5efcb0f911f3e4462e8538eeeecf62d9f9d
+size 608
--- FrontEnd/src/components/Aichat/ChatReport/ChatReportChart.tsx
+++ FrontEnd/src/components/Aichat/ChatReport/ChatReportChart.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8397d16812beb238928998efee0419ebee56ca6b353b1fd069f7a38215d7314
-size 983
+oid sha256:8b9e441eac98b8c57b54aa286ffea780b0eea197d14b64103d01a07f71dcb41d
+size 1602
