01df4e91bace9c9c587468f07d3ac26c1d03821b
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-28 11:30:58 +0900

    feat: 이메일 관련 기능 swagger 설정 및 자바 docs 주석 달기 처리
    Former-commit-id: 8487362b5e4519eca6c43bd6bf160cc37b33cac9

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/email/service/EmailServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/email/service/EmailServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ec94645018fba825213fe4c9e2075ed9cc549a874986d123a6bced1f65eab47
-size 3700
+oid sha256:20adfbc39a78dfe722d4bdd13bfda5e82e98853eee89b6a6da19cfd932fb7d13
+size 3698
