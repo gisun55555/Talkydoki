905ed8311b4e4751c43ca90fcdf5f414bb33a726
백민정 <peku9693@gmail.com>
2024-03-28 11:34:24 +0900

    fix: 사용자 뉴스 데이터 없을 시 뉴스 캐러셀 미표시
    Former-commit-id: c516829e8069fca3e72ffcbf26d851c8383e40b8

--- FrontEnd/src/styles/News/Detail/container.tsx
+++ FrontEnd/src/styles/News/Detail/container.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0df5182dfd79f76c72000a301d81ef90c50d2a54a94b89faf1dc53e01220c01
-size 4822
+oid sha256:cd80eeb155bbc19efe82d70e15ae59ac468a9ecd6f068566caeb56e61f1febba
+size 5190
