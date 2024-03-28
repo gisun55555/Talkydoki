6bf3d35b1ec805d25d889e371671fe3e33f0c89c
오기선 <pakox123@gmail.com>
2024-03-28 16:26:22 +0900

    feat: ai 채팅방 연결 api 제작 및 연결
    Former-commit-id: 60bec8d37f183143022258c8eabac89d3a22c5d3

---
+++ FrontEnd/src/api/chatApi.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e3413e32a86dae0eb4daa5c8a593acbaa05bc1491ca2a877bfd29ed2b4bac961
+size 1364
--- FrontEnd/src/components/Aichat/ChatRoom/ChatRoom.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatRoom.tsx
@@ -1,22 +1,3 @@
-import { ChatRoomContainer } from "@/styles/Aichat/AiChatRoom";
-import ChatHeader from "./ChatHeader";
-import ChatMain from "./ChatMain";
-import ChatFooter from "./ChatFooter";
-import ChatTip from "./ChatTip";
-
-type Props = {};
-
-function ChatRoom({}: Props) {
-  return (
-    <>
-      <ChatRoomContainer>
-        <ChatHeader />
-        <ChatMain />
-        <ChatFooter />
-        <ChatTip />
-      </ChatRoomContainer>
-    </>
-  );
-}
-
-export default ChatRoom;
+version https://git-lfs.github.com/spec/v1
+oid sha256:9998194e8e30bb632ef34b1c1e1424468fae58388fd935ee027da519a6b9747b
+size 2459
