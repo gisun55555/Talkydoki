64ccba80a5f94aa535589af68f316455a63541e4
이지호 <jeho1129@naver.com>
2024-03-28 09:24:12 +0900

    fix: 백그라운드 갱신 시간 10분 -> 5분 조정
    Former-commit-id: efb489320a29cd11a68b347f464e043b4c81e7fc

--- BackEnd/FastApiServer/recommendation/recommendation.py
+++ BackEnd/FastApiServer/recommendation/recommendation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:344c66de791d29c461b592e9eea6a388ce657665192f3c6ca3225f59d27c17ce
-size 8144
+oid sha256:f416d38702b975d977e9393e6b952a8b531b73a7b42e979e947d4fc9d0bf90b3
+size 8143
