58b66ea290b9194b23da026393caeb95f67d1da2
오기선 <pakox123@gmail.com>
2024-03-28 16:28:55 +0900

    feat: tts 채팅보내기, 소캣을 통해 채팅 받기 api연결완료
    Former-commit-id: 82ea5568d009da6d1cd7913cf809fab2bb894f7f

--- FrontEnd/src/components/Aichat/ChatRoom/ChatMain.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49c32dc367d509a29e88e25c4794eaf8a7d89b028c3de0fb9783ba4472b5ac14
-size 2274
+oid sha256:774bfb6559430cfe475aba6f576f2f5b121aa55df214a2f27df4207d4a3b369e
+size 1470
---
+++ FrontEnd/src/interface/ChatInterface.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd980a63953a150d60d77c0d51d64cdcbf6f2cea12ab5e097e9e3fe01b2524c7
+size 426
