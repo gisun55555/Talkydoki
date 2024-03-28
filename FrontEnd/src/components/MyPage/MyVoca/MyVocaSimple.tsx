af05db312ecc00d59aec0e3f947519ca212932ab
송강산 <rkdtks0816@naver.com>
2024-03-28 00:54:28 +0000

    fix : npm run build 에러 수정
    Former-commit-id: eef688a57ae3edc504a33b22ac132563bf8f7b43

--- FrontEnd/src/components/MyPage/MyVoca/MyVocaSimple.tsx
+++ FrontEnd/src/components/MyPage/MyVoca/MyVocaSimple.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8069f39dc42d1092f669252af58f701e28647fdbf7ed47097c7a4bc455bc4b74
-size 2200
+oid sha256:d79de167358a458fa6a5668c129d45b8ceeae85b3d7ce2aee6d82a9cafff4530
+size 2219
