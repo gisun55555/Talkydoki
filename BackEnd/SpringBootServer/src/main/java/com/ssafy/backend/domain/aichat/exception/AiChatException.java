0e5e3473e340b686a0d3f8b3ed8ec02e0d297eb8
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-28 10:59:02 +0900

    feat: AI 회화 채팅 커스텀 Exception 처리
    Former-commit-id: 40f6e219a8e74edc74ca0a606cb9fb9f2eeb3349

---
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/exception/AiChatException.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dfa9aa4f742d7cc951ed8b94983062f88922b2495991c9cc6afb9d168689c22d
+size 329
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a34c3bd568803c46f3b941d6bbf41b8479dd418d8a71398860a38364f6dc442
-size 16373
+oid sha256:bbd874db962fc79100545b9b7c944bd9af805d998d176490213dd5f6c0fc7fc8
+size 14241
