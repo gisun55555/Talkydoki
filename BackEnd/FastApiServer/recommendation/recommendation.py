eb84d053545998a1e583feb7712d746f6a352ef4
이지호 <jeho1129@naver.com>
2024-03-28 11:38:12 +0900

    fix: News Image 없는 뉴스 데이터 접근 시 Deadlock 에러 해결
    Former-commit-id: 410a1a4f7ff4684d14b5923984c2b85f1b8b2f4a

--- BackEnd/FastApiServer/recommendation/recommendation.py
+++ BackEnd/FastApiServer/recommendation/recommendation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35f042d35c34db6105bb3a2c6ecf2902af4f08df95d13e6a52d2624ea0efffa1
-size 8171
+oid sha256:b500644c92cb2427737975f43012125d7f81d3e7b98b1dd1b5286be361044365
+size 8213
