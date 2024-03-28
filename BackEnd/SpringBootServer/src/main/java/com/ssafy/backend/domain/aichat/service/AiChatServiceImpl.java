8cc1151b5a820d8e57032a20dd7e3cb9fcf661b1
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-29 02:32:22 +0900

    refactor: AI 채팅 회화 메서드 단일 책임 원칙 따르게 리팩토링
    Former-commit-id: c890a28292819a40cfc02d553c8e75e07705b636

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9eb14110e4b1966cb7470c12db421eb561455c8fced3d2efa442a93f117885a
-size 12797
+oid sha256:765336faff241dd776d519ecc0334d52f38deeddc783872f7deb826ab2c97100
+size 12212
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/dto/Conversation.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/dto/Conversation.java
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8ac918e327b38e92cc3bd0d5a3ff5b83e0d3afb4e2a4726bef75c196a75d483b
-size 418
+package com.ssafy.backend.global.component.openai.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record Conversation(
+        @JsonProperty("gpt_japanese") String gptJapaneseResponse,
+        @JsonProperty("gpt_korean") String gptKoreanResponse,
+        @JsonProperty("user_tip_japanese") String userTipJapaneseResponse,
+        @JsonProperty("user_tip_korean") String userTipKoreanResponse
+) {
+}
