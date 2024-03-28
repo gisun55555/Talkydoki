f9d9f1d145e45f5b013f600f475147c332c253e2
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-28 15:12:05 +0900

    fix: GPT setup 프롬포트 수정 및 일관성 수정
    Former-commit-id: 49ba80daf13e5dda238a8660b95f4308b867eeb6

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/controller/AiChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8dc1ea39b31ac4e18e82bcad30ed4ee7754cdaaa5d7e9b25ed8886545ebf68a
-size 3247
+oid sha256:5ad474e59f278edded5712947a5049cb4c0bd95782451702eb7bc0eeff05eb8e
+size 3115
