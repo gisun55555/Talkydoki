1cf3816c4b4bf3ec8d338296619b4ea04ef05b86
이지호 <jeho1129@naver.com>
2024-03-28 10:11:30 +0900

    feat: 사용자 관심 키워드 API 구현
    Former-commit-id: 8a7210a3f6d8274490c26daad1ea5a5683f39b6f

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/repository/NewsKeywordHistoryRepository.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/repository/NewsKeywordHistoryRepository.java
@@ -1,12 +1,3 @@
-package com.ssafy.backend.domain.news.repository;
-
-import com.ssafy.backend.domain.news.entity.NewsKeywordHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NewsKeywordHistoryRepository extends JpaRepository<NewsKeywordHistory, Long> {
-    boolean existsByKeywordId(Long keywordId);
-
-    NewsKeywordHistory findByKeywordId(Long keywordId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b5eda880df0352b9c37d4d975b5656262553f55d3caad6657d64c167229291b7
+size 547
