888afb5e6e9bd7dc888002f76e0340e08bc765bc
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-29 02:57:01 +0900

    fix: AI 회화 채팅 서비스단 자바 docs 처리 및 안쓰는 코드 삭제
    Former-commit-id: e025d0712c4d10982c94a25a1a29c9fbd0897606

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatService.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4928d3cdf63458560726d109daa194a58fcf75f67ca88c348080bccd5b7daa4a
-size 1666
+oid sha256:92c57ab6799e24c5edaf40cb97109cb5cbae302de897b6fd07e8373de243372a
+size 1208
