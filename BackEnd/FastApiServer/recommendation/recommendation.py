fe85dbeaea6a06d5eb5464e1699efe5bcc5d90fe
이지호 <jeho1129@naver.com>
2024-03-28 10:49:06 +0900

    fix: 뉴스 추천 시 해당 뉴스 이미지 없을 시 빈 리스트 반환하도록 수정
    Former-commit-id: b6016973e690101832cec6d8138a2bcdc901807a

--- BackEnd/FastApiServer/recommendation/recommendation.py
+++ BackEnd/FastApiServer/recommendation/recommendation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f416d38702b975d977e9393e6b952a8b531b73a7b42e979e947d4fc9d0bf90b3
-size 8143
+oid sha256:35f042d35c34db6105bb3a2c6ecf2902af4f08df95d13e6a52d2624ea0efffa1
+size 8171
