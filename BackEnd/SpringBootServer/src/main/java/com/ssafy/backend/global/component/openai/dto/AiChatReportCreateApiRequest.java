c89eecd5291f792bf50b959ba6d506f174e4da17
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-28 14:30:53 +0900

    fix: 백엔드 build 오류 긴급 수정
    Former-commit-id: 16bc61cdb7f85b03d900ce8c046aa938ca114999

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/dto/AiChatReportCreateApiRequest.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/dto/AiChatReportCreateApiRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d99988ac7a616599a61c342fb600d93ad41d2acdedb6a2b0fa7ea46a68dfb1f
-size 11324
+oid sha256:af61fc9a92ba7481c82a169fab46219f5d9716738e9980e9a2924ec97722b29a
+size 11363
