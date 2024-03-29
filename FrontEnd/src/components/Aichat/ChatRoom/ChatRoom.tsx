4d80884a12de8a87abb11acd495099106de35130
오기선 <pakox123@gmail.com>
2024-03-29 10:20:19 +0900

    fix: 채팅방 api 수정
    Former-commit-id: 7a769604f86d801cede470295298ba424e4a50de

--- FrontEnd/src/components/Aichat/ChatRoom/ChatRoom.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatRoom.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9998194e8e30bb632ef34b1c1e1424468fae58388fd935ee027da519a6b9747b
-size 2459
+oid sha256:49d5c84cbd02b4fc65884650e199555a99602e6d2bdded4a0af47cc01ac797db
+size 2602
--- FrontEnd/src/util/websocket/stompConnection.tsx
+++ FrontEnd/src/util/websocket/stompConnection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af4d3c1c8e8be18f242148f81c48f352969080809cf5c76d4bd8e3b7ca6a092f
-size 1169
+oid sha256:5f2dc53851c080957c5093b319b05b74b81e11e5bf3fff16d3f4bccf3dbaf814
+size 1313
