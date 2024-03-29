6ef23eb06dcaf12ec681ea9ae96602c4bdc252fe
오기선 <pakox123@gmail.com>
2024-03-29 11:39:28 +0900

    fix: 필요없는 코드 제거
    Former-commit-id: a499169832fd389cc47df2b1914e73c8ae9d105e

--- FrontEnd/src/components/Aichat/ChatRoom/ChatRoom.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatRoom.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49d5c84cbd02b4fc65884650e199555a99602e6d2bdded4a0af47cc01ac797db
-size 2602
+oid sha256:35742ad9c79d4d7c68d896e1dfafe0ce43eab9fc97ce4f13718c1c63d0b33e46
+size 2568
--- FrontEnd/src/components/Aichat/ChatRoom/ChatTip.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatTip.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a160af6e3fb3c31e07048a61de616b527d5c131077879a6542974eb4749c8ce3
-size 3279
+oid sha256:de4f0ab77eaedf11c3566d2d6bb818cb04299c41776623398e685d8fec9b788c
+size 3267
