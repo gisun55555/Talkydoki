de27be39156d6acf8f28f303921e01b37d46e05b
조현호 <chh5680@naver.com>
2024-03-29 22:46:02 +0900

    fix: report와 피드백 조회할 수 있게 유저별 레포트 목록 조회 시 rooId 정보도 같이 전송
    Former-commit-id: 6620e2eb35c30333850434c042b520d65d5e34bf

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/dto/AiChatReportInfo.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/dto/AiChatReportInfo.java
@@ -1,9 +1,3 @@
-package com.ssafy.backend.domain.aichat.dto;
-
-import com.ssafy.backend.domain.aichat.entity.enums.AiChatCategory;
-
-public record AiChatReportInfo(
-        Long id,
-        AiChatCategory category
-) {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:14d07ce396c9c948dc014f6cd5c520d8ea333f817f5f0364e9bdd567d658474b
+size 229
