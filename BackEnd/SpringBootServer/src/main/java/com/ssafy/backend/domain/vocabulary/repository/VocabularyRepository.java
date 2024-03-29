917780d19ec3b25fc84df1f36de88b76b9b1268e
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-29 17:14:56 +0900

    fix: 해당 단어 검색하기 쿼리 네이티브 쿼리로 변경 (해당 단어 중복될 경우 하나만 가져오게끔 수정)
    Former-commit-id: 1064cc3d2d95633c58c1e72576a15b59e609d783

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/repository/VocabularyRepository.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/repository/VocabularyRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e736ef175923ed88ebbeaf56e8cb3c22e3d483fd41052873bbb112ca6e0144a1
-size 656
+oid sha256:1a683a084cd9402ae7ed90e5c27ac07a1bc00d7073938ef48432e95b16ba18ba
+size 813
