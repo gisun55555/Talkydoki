6f137cada5a01fa85f0bd0c0a88bd20829e4e559
오기선 <pakox123@gmail.com>
2024-03-29 22:33:38 +0900

    fix: 깃 충돌 해결
    Former-commit-id: a197f46432dc2d80f6a44d806285a3e1feee3a1c

---
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatMessage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ce7dabc2f17da0488065feaa2a7c8aadc9485ca19463142b50313f2b6f561b4c
+size 2982
--- FrontEnd/src/components/Aichat/ChatRoom/ChatRoom.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatRoom.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6576e41223540ce08b0c4d647a2cbcfe8089d52291ad6202134607f50d16e66
-size 3200
+oid sha256:35742ad9c79d4d7c68d896e1dfafe0ce43eab9fc97ce4f13718c1c63d0b33e46
+size 2568
