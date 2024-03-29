201890f8f7ebb1d2c1b2e3bc3d33cac0b028fe17
tlsehdrms124 <tlsehdrms124@naver.com>
2024-03-29 12:00:45 +0900

    fix: AI 회화 채팅 대화 주고받는거 순차적으로 DB에 저장되게끔 수정
    Former-commit-id: 569bd2bc119f9fc860663acdbd216771edd81ba1

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83995e1636e31c1e8b47fcce1886ab0b5efb3d8fdb436441ee5e510a8aa43bb1
-size 15418
+oid sha256:f8dcf931c02ecde525a52548676c03557569c1a3e3dab9724f18ad57e759ecdc
+size 15537
