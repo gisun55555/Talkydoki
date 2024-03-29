c7e368875d719206596947d65b337c5d5f45bf0e
송강산 <rkdtks0816@naver.com>
2024-03-29 17:38:23 +0000

    fix: 번역, 날짜 형식 변형
    Former-commit-id: b52bc7a9b533cbec6da2339480a764bc2d2d55b1

--- BackEnd/DataProcessing/NewsCrawling/NewsCrawling.py
+++ BackEnd/DataProcessing/NewsCrawling/NewsCrawling.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:297bbcc9011b88edb89efc4c073c9e7c27140ed81a260e2220d0990b5477e324
-size 7618
+oid sha256:7d8694ab10faafce72abdcd67512ff237d7e692d283afb207291ac6e41acbeb3
+size 8577
