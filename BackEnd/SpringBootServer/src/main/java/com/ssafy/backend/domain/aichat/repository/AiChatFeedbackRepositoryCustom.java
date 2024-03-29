0606caf2b238799dea87f61f98247766ccb0d06c
송강산 <rkdtks0816@naver.com>
2024-03-29 17:38:23 +0000

    fix: 번역, 날짜 형식 변형
    Former-commit-id: 038e3a3bfc936adbc891b202bca1db2f4ee8a3f4

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/repository/AiChatFeedbackRepositoryCustom.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/repository/AiChatFeedbackRepositoryCustom.java
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:19c24b0d39911fe0c5bd04c7b264cd8a7761ebb557aa0ebb26bbbab97ca8b378
-size 265
+package com.ssafy.backend.domain.aichat.repository;
+
+import com.ssafy.backend.domain.aichat.dto.AiChatAndFeedbackInfo;
+
+import java.util.List;
+
+public interface AiChatFeedbackRepositoryCustom {
+
+    List<AiChatAndFeedbackInfo> getAiChatFeedbackInfo(Long roomId);
+}
