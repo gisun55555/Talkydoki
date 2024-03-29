56c169c205bfa46493a98bd1208b62f1a3aa1f33
오기선 <pakox123@gmail.com>
2024-03-29 10:21:08 +0900

    feat: 채팅방 css 수정
    Former-commit-id: 180904f863ab94ed9853e0eec46fa0c2ba1fe0ce

--- FrontEnd/src/styles/Aichat/AiChatRoom.tsx
+++ FrontEnd/src/styles/Aichat/AiChatRoom.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c1ccfdcf0567abbe0f8f8622a93d9bc0874f764e86d2909a8fe9fc782bf1af2
-size 5015
+oid sha256:a0511f1157e2e8bdf8b778cf1ebd66255e8192d739bce12eb9ef48bacbef1d56
+size 5527
