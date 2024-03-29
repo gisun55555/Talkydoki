cdc8c77b26a5d3efcc15b0148d2ab88c9136e44a
rkdtks0816 <rkdtks0816@naver.com>
2024-03-30 01:59:37 +0900

    feat: 날짜 형식 변경 초까지
    Former-commit-id: 4f01a4a26f53a29632aa761dc94b3f14a226449f

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/NewsServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/NewsServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8b9d90194ed1db3a4df22b1c7f3a4cf001ccccbd06ba569e77346d70af0b2e5
-size 7689
+oid sha256:5231dbbec2e4b95829306d3c30f83665db430e2290f713b303ba87aa55bba272
+size 7612
