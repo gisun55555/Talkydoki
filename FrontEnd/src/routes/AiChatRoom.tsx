a8c3263a1f97fc88be1dfb38dabb3001ce1adb0a
송강산 <rkdtks0816@naver.com>
2024-03-29 14:10:16 +0900

    fix: 웹소켓 nginx, 데이터 프로세싱 dockerfile Former-commit-id: f39c622a4cc6a1901c724f7257d427d03acc97d4

--- FrontEnd/src/routes/AiChatRoom.tsx
+++ FrontEnd/src/routes/AiChatRoom.tsx
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:86e20425865d2c19b8b2aacb52b3a1117e6f93b9efb331d346e4100a764fe3c8
-size 342
+import ChatRoom from "@/components/Aichat/ChatRoom/ChatRoom";
+import { ChatCard, ChatWrapper } from "@/styles/Aichat/AiChat";
+
+type Props = {};
+
+function AiChatRoom({}: Props) {
+  return (
+    <>
+      <ChatWrapper>
+        <ChatCard>
+          <ChatRoom />
+        </ChatCard>
+      </ChatWrapper>
+    </>
+  );
+}
+
+export default AiChatRoom;
