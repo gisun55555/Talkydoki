c2fbbf4dd5676c35e1c89201c8431a90da538cba
백민정 <peku9693@gmail.com>
2024-03-29 12:13:48 +0900

    fix: 뉴스 타이머 오류 수정
    Former-commit-id: 10eeb19144a4fe62666356d499e49c48be6fe101

--- FrontEnd/src/styles/News/Detail/container.tsx
+++ FrontEnd/src/styles/News/Detail/container.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb87d9d9972f7b31c82ad672c783ef16153db33be1435db71fa4fd9be223891f
-size 5256
+oid sha256:981f29ed3e2ea228edb3bc7af91b631d962953ebc75416fc9602e4d1cdf33819
+size 5433
--- FrontEnd/src/util/language/japanese.tsx
+++ FrontEnd/src/util/language/japanese.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90470597e51f0290414ff3ce4501242aae676da181ca16b2de97b12c1244b4bc
-size 3159
+oid sha256:1c69e39f45c02e46eb2a6d93bb3912e2900f9edab9d2fa384abfcc37b2a59bdd
+size 3531
