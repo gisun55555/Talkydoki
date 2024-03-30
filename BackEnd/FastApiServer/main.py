e382cfef351da28d9835bde425f72f13fd601941
이지호 <jeho1129@naver.com>
2024-03-30 17:23:54 +0900

    fix: requirements.txt에 PyMySQL 추가
    Former-commit-id: 4d5d0c631991ffc4f3edf631c7ae6506cad8cac4

--- BackEnd/FastApiServer/main.py
+++ BackEnd/FastApiServer/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7219af60add7d7d24ffc4896d7559d5bc31d2fbeacd0b683e347d7f5dcc406fb
-size 2150
+oid sha256:64ae75b85dcc220d6312a29ddb7fdc61f247b0552ae35c87a973921432bc99c7
+size 1853
