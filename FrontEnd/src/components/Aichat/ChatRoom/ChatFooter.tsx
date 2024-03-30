f822469aa3a3f807b2b48cb980aaf02b528b6831
오기선 <pakox123@gmail.com>
2024-03-30 17:07:19 +0900

    fix: 녹음 오류수정
    Former-commit-id: 30c16e4b6d4fb83a9a193343ce7bda8e4268a7fc

--- FrontEnd/src/components/Aichat/ChatRoom/ChatFooter.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatFooter.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:674bee02036b31b20393da885f447195493c1f935dc49e69e55e4bcc4724839c
-size 4955
+oid sha256:b2bcd5d52d24c922c020607cc2263965dad922e950eda8cb7f7ee96d0f11d7a2
+size 5284
