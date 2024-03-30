63c49a903cececbdd5947071b77b9c8386e0ec16
송강산 <rkdtks0816@naver.com>
2024-03-30 06:22:31 +0000

    feat: 더보기 버튼 클릭 후 크롤링 기능
    Former-commit-id: efa4a4f357d3a6745a3f7774544771f5236c011d

--- BackEnd/DataProcessing/NewsCrawling/NewsCrawling.py
+++ BackEnd/DataProcessing/NewsCrawling/NewsCrawling.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d8694ab10faafce72abdcd67512ff237d7e692d283afb207291ac6e41acbeb3
-size 8577
+oid sha256:8575d0d7cf4bf73f2475d7869bec606d217ef960a1bee40a1d9dfbe0cf84b092
+size 9093
