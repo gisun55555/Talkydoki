d6ccbeaa218c5d382334445a4f73293abe1d00bb
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-01 02:33:26 +0900

    fix: 로컬환경 db 백업본 ai 채팅 회화 테이블 컬럼 enum 값들 변경
    Former-commit-id: b4c9b32ae75f2fb8a7b5042f544c0f43d0ece7b9

--- BackEnd/SpringBootServer/src/main/resources/db_backup.sql
+++ BackEnd/SpringBootServer/src/main/resources/db_backup.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f4410cb7572d05170b34bf611529546df7496e60fd6b93d1ab3ec9f211b627a
-size 13751112
+oid sha256:1c13c975efc8e450eeb993e9506cb0484835ab37ffec4aac651616b6c7b3b62e
+size 13751286
