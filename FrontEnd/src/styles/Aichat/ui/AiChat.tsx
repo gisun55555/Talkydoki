a724fd9c005e193b190714d964f6ae63031eaa9a
rkdtks0816 <rkdtks0816@naver.com>
2024-04-01 00:19:55 +0900

    feat: 채팅관련 컴포넌트 생성, 채팅방과     CSS
    Former-commit-id: a3a256f9e01aa282eb5bcf3434add103383792eb

--- FrontEnd/src/styles/Aichat/ui/AiChat.tsx
+++ FrontEnd/src/styles/Aichat/ui/AiChat.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a76bf22c9ea24d54ed15af2fe7b9aba8582dcf5e9bf868b24e8d5dd8af0d7dc
-size 2799
+oid sha256:a95a8d1ca9b263010818d4f74e001f9d128090b963076bd4dfcd958f703cf0d8
+size 2800
