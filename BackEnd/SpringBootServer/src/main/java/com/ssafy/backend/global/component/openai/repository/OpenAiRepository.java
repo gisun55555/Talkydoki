f589300b4a228a9cc7984dad5e867aaa8bac7dd1
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-01 15:51:17 +0900

    fix: AI 채팅 회화 재시작시 redis에 캐싱된 값 삭제하게끔 수정
    Former-commit-id: eea86c2d83dd6238becf10023a569dc4207bc0b1

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/repository/OpenAiRepository.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/repository/OpenAiRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c3184efe354d7b8cf53982022f430a3235f794957a917a2e78abf8a8b63e6dd
-size 2866
+oid sha256:8510098335503bcb351a99deda5369255a5d2fb89df4fb46a89885516b4a7846
+size 3064
