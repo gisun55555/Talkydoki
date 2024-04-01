6d0453a89525d1c85ca9734852185f386df7d6d1
오기선 <pakox123@gmail.com>
2024-04-01 15:36:25 +0900

    feat: AI채팅 전체 목록 확인 버튼추가
    Former-commit-id: 3fdfa9a0c01af6faf72a8e44b34e375da4957537

--- FrontEnd/src/styles/Mypage/ui.tsx
+++ FrontEnd/src/styles/Mypage/ui.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69932cef287c49aab95c8492d8ea681dff511ee79e6d820935c0253fa2d0aeb8
-size 1998
+oid sha256:7f1e29e2a1527b848f7a5de11c339d9a61c95a95d2ccd85bd2b68873002e0ebf
+size 2666
