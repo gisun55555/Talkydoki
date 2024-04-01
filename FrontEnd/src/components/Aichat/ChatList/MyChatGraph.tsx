5339368c8b0db13d876333aba46d053f3f779d8b
백민정 <peku9693@gmail.com>
2024-04-01 18:01:38 +0900

    feat: 평균점수 추가
    Former-commit-id: 397f98260580e0859a9aa9ee3d7b0004ca43120c

---
+++ FrontEnd/src/components/Aichat/ChatList/MyChatGraph.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ea2f12bd2b1d7047f0d6524f5eff849ff8010ebe337d5ee60bf75fdb88ae9719
+size 3814
--- FrontEnd/src/routes/AiChatList.tsx
+++ FrontEnd/src/routes/AiChatList.tsx
@@ -1,17 +1,3 @@
-import AiChatCategory from "@/components/Aichat/AiChatCategory";
-import { Flexbox } from "@/styles/Aichat/AiChatList";
-import { Wrapper } from "@/styles/common/ui/container";
-
-type Props = {};
-
-function AiChatList({}: Props) {
-  return (
-    <Wrapper>
-      <Flexbox>
-        <AiChatCategory></AiChatCategory>
-      </Flexbox>
-    </Wrapper>
-  );
-}
-
-export default AiChatList;
+version https://git-lfs.github.com/spec/v1
+oid sha256:6d051e560449799ce0ebc4fc2cd602ee35bec3df57ec63ee5879bd982ae1ce43
+size 467
