ef56cf3d97c4fc4ae62978f4678835979653a636
백민정 <peku9693@gmail.com>
2024-04-01 18:01:38 +0900

    feat: 평균점수 추가
    Former-commit-id: 5d0f9fa64041614edfee8f1d665bdde1de88a393

--- FrontEnd/src/styles/Aichat/AiChatList.tsx
+++ FrontEnd/src/styles/Aichat/AiChatList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a550cfdf61d856988ad33cc25600ec3d53a858b2a86710a608b4cfe8669e127a
-size 1206
+oid sha256:3c1b04daf54250ec0fe4d4ff64b177690df03ac5d0979b9d1a12fc4808a274a1
+size 2419
