2b28b0c8206c76d5a3a54d2ccd1f0c6318656963
백민정 <peku9693@gmail.com>
2024-04-01 12:08:30 +0900

    fix: 빌드오류수정
    Former-commit-id: 955209021b5a138000ee4f3dbfbcc7f3807d6146

--- FrontEnd/src/components/MyPage/Jandi/Jandi.tsx
+++ FrontEnd/src/components/MyPage/Jandi/Jandi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8d14081423135f3489adca01bbccd2c28c1326678e851dc98da5300e2711195
-size 2534
+oid sha256:e2efa5db3f7c66fd5af3b40852f213d56f9ffa3aff9be5bc6333d7e61e110432
+size 2558
---
+++ FrontEnd/src/styles/Aichat/ui/AiChat.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7bbb8ebb85562c1798f2790eea0ed2dd6002ada2f854ce3a006e8f251b77b104
+size 2976
