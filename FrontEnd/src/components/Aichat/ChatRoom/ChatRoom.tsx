e38f32b4c78934d39bb2890b5833496edcaa0739
백민정 <peku9693@gmail.com>
2024-04-01 16:54:10 +0900

    fix: 메인화면 캐러셀 이미지 안뜨는 문제 수정
    Former-commit-id: 23522d560ea70fdbe5458a1bc3998015259fb717

--- FrontEnd/src/components/Aichat/ChatRoom/ChatRoom.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatRoom.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4625212efd9fcc5dc7490aef030b41144e5421f8bdf5113c6970c00f497b5c0
-size 3661
+oid sha256:4518a67d1fd5d25b2dc6bcf10ecc4de832189a3d5e8321ac02da50cc42fd4f93
+size 3647
