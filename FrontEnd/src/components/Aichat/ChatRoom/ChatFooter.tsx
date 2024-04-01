6a37bcf31b5d68501284a8acf9ff1cd67fb0ee72
송강산 <rkdtks0816@naver.com>
2024-04-01 11:14:31 +0900

    feat: 채팅 리포트 페이지 수정
    Former-commit-id: 3a8c87a9f9835074ed4eb43afca4796bcf85fb61

--- FrontEnd/src/components/Aichat/ChatRoom/ChatFooter.tsx
+++ FrontEnd/src/components/Aichat/ChatRoom/ChatFooter.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:baad0a14d1b5c7501241bcb67f78185e405227393ac9288da0b00dd13146d7a0
-size 4711
+oid sha256:d9403ee9cc156647044efc645047072bb9c738ef92f5bf705114c15dd1123154
+size 4741
