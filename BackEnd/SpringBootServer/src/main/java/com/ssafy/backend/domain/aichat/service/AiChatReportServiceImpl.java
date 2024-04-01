78703717cbf52965646f4196c36df7441b664663
조현호 <chh5680@naver.com>
2024-04-01 10:49:09 +0900

    fix : 유저 별 전체 레포트 조회 버그 수정
    chatRoom 에 대해 report nullable이라 <optional>타입으로 null 처리하여 해결.
    
    
    Former-commit-id: 349aa257f9104737efabd41b865d9c3945c97892

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatReportServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/service/AiChatReportServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:093b4cdae51d2da286a9ddf9a294dc260866b66b940f049c26c8959bef5b1590
-size 7680
+oid sha256:310809adb356de48bd5d1487ad13172e0dd36c3ce51e063a5f5f357ad9bb1cf4
+size 7489
