62d36cd6e97bd9869a83ba70f09c6ad339920018
백민정 <peku9693@gmail.com>
2024-04-01 16:54:10 +0900

    fix: 메인화면 캐러셀 이미지 안뜨는 문제 수정
    Former-commit-id: 85abcb4e622e8ff29a4217e10c31a23459cb066f

--- FrontEnd/src/components/MyPage/Jandi/Jandi.tsx
+++ FrontEnd/src/components/MyPage/Jandi/Jandi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2efa5db3f7c66fd5af3b40852f213d56f9ffa3aff9be5bc6333d7e61e110432
-size 2558
+oid sha256:2498aef9d44a41d432cc042b6f783fba582922e0f8cf458f16445d3f6db3051a
+size 2074
