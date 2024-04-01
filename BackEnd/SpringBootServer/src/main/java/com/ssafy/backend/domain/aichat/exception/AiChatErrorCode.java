13b0a13dd897d122773599506080e0ff68ed728c
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-01 17:29:26 +0900

    chore: 오탈자 수정
    Former-commit-id: 80b6cc8959d2f0143ccdcb137fbdfb81c2f1db05

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/exception/AiChatErrorCode.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/exception/AiChatErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e739e2710185ebef2898712d1e7b0aef9ed1b42a2a84b5d260b8d3b34109210f
+oid sha256:e21de9d9a9580996526f2794f82ab51c7688e3b48e77a45adb1d7c76b76b14d7
 size 695
