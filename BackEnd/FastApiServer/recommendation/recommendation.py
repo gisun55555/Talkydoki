731cc10ec33d0f2aef7e2c15729379294af3f41f
이지호 <jeho1129@naver.com>
2024-04-01 12:18:25 +0900

    fix: 뉴스 추천 위해 Data 로드 시 에러 발생 수정
    Former-commit-id: 797a2d4cf2257eed694e6c625c11cf67cfcc2d42

--- BackEnd/FastApiServer/recommendation/recommendation.py
+++ BackEnd/FastApiServer/recommendation/recommendation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99cfc31b78e3f62aa8d807c9a53ff6d43496bb0eab40f6e8788995ddd942ac20
-size 9969
+oid sha256:61ba86fcf79b97f2f8a4bc4db89c5c255e0611c6a3ec04e612d3fac9f0f4d821
+size 10359
