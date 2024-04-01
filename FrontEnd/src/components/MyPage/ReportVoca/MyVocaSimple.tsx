0881bd0bc3078c1924ce200732499a2fcc3dbb64
백민정 <peku9693@gmail.com>
2024-04-01 15:43:58 +0900

    fix: 단어장에 데이터 없을 경우 없다고 표시
    Former-commit-id: 12ce5b7d0adae86396199a81f07a4ce066ce5882

--- FrontEnd/src/components/MyPage/ReportVoca/MyVocaSimple.tsx
+++ FrontEnd/src/components/MyPage/ReportVoca/MyVocaSimple.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:421513c0b3999e8fa76d4d53433a0b79da1966d2beaf4cef31208a59e8e30953
-size 1418
+oid sha256:7f4b36ef22d2d0287d002f3eb1e3878e6104ffa7d0f6ea3ae2968eb4c527834a
+size 1533
