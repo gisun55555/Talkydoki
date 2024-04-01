1b097c8853154372ae489263cbd4d5bcaa6b570c
오기선 <pakox123@gmail.com>
2024-04-01 15:36:25 +0900

    feat: AI채팅 전체 목록 확인 버튼추가
    Former-commit-id: 792257c3105da35e504c2ba192d10d3201937311

--- FrontEnd/src/components/MyPage/MypageHeader.tsx
+++ FrontEnd/src/components/MyPage/MypageHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d837fb7db2cc96ae4b663d496b77653ef07214de0e0b31944147bf1b3e24e198
-size 1165
+oid sha256:c26613c4da4f58859be12214e236b1a4594a6ae5509ca12ff32aea0b04e308c5
+size 1410
--- FrontEnd/src/components/ui/Category.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fd32cf07f303a5147b4b3f7e28a19ac08bd8c24ba2d65c708830a47160e867fe
-size 1499
