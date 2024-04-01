2f2d7f748d3b314735ce257771808ae5e0bc30ec
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-01 11:23:20 +0900

    refactor: 이메일 인증코드 발송 비동기 처리 및 이메일 형식 안맞을 경우 Exception 처리
    Former-commit-id: a1e630bd95e6493e601ee1c312e3b085b62024f0

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/email/controller/EmailController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/email/controller/EmailController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1b043b7357cb53c975deb8f5db7ebe9d3087c4f5f8eed08d2c3e7f5db26ba5c
-size 1837
+oid sha256:b78bbb421c63330356d23d396dffbaab1bbdeb7e1d863e3ba53baceed7d687be
+size 1904
