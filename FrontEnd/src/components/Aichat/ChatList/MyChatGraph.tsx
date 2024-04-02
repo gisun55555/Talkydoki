37321cc3fc30920d30715e7bd47ee820098b8e22
백민정 <peku9693@gmail.com>
2024-04-02 09:39:23 +0900

    fix: 그래프 오류 수정
    Former-commit-id: 629109057b139a5aaad74507b4f0e3f7149be7a2

--- FrontEnd/src/components/Aichat/ChatList/MyChatGraph.tsx
+++ FrontEnd/src/components/Aichat/ChatList/MyChatGraph.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4aad3c369a7898562120037d480703b3b770858a6b0df26c147c0f7623e505b0
-size 3691
+oid sha256:aec9846ff65ed7cca8062fd40555d9856603c08560fc50efdc1d8fc2034f7c42
+size 3872
