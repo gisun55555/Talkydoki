775386b5625ba7ed77c91ed22732d993a0ceb372
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-02 10:50:34 +0900

    feat: 나만의 단어장에 추가하기 성공 응답값 변경
    Former-commit-id: 40c23803b87311555140471324c72d607584dc6d

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/service/VocabularyService.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/service/VocabularyService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdf38e2e721c83675964cb98e7879aeb7d7928daeea44d710c1881eda2870a14
-size 1010
+oid sha256:94776169a6b02b4ca5af5afa1c124e99347c80ccdaa867b0f405f5f229cb2047
+size 1028
