0b2a4b533114988a9aba2b23f3384928f9d9f5b9
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-02 17:49:56 +0900

    feat: AI 채팅 회화 category 컬럼 마이그레이션 (컬럼 추가)
    Former-commit-id: e348a58a74b92aea0b7481f5a58d912888cea052

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/entity/AiChatRoom.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/entity/AiChatRoom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a30b49893e9e780c01f5809f61126443f557bb14c4647c14c3caf756ee126bcc
-size 735
+oid sha256:3f45807e594dd5fb2dbb52a08114bda3db7f422cd6757d6ce5ad86e9da901cf3
+size 672
