ce65b51a452196afb5f7c552a9f1c70699d1c0ef
오기선 <pakox123@gmail.com>
2024-04-02 21:40:50 +0900

    fix: 카테고리 호텔 체크인 추가
    Former-commit-id: a224c10d4ee48c086321824c7132846ce7c71bee

--- FrontEnd/src/components/Aichat/ChatList/MyChatGraph.tsx
+++ FrontEnd/src/components/Aichat/ChatList/MyChatGraph.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2916a16d28b2457a438370a1b4f721d75946b17b7fed5d2f77bd471f46e34138
-size 4322
+oid sha256:8ffbdfaa800975f186f807c083309e0994684a80b71b1a03d0d9a8b8e6a1c23e
+size 4303
