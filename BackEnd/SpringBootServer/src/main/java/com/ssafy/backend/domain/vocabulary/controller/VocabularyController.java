0310032681dcb1db768f5f4c1a1235fa6113bc0c
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-02 10:01:46 +0900

    feat: 랜덤 단어 가져오기 및 해당 단어 검색하기 기능에 나만의 단어장 아이디 컬럼 추가 및 로직 변경
    Former-commit-id: b54c211d7c2f237285105ab3d74dc965077092e4

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/controller/VocabularyController.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/controller/VocabularyController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7910562cd870a7063050ce3d4cad610f274f2e65d0cb3de86d8b2c5eee38ec85
-size 4060
+oid sha256:30082dba0cde48238f31e2fbeff1920b7392809856b9105782977ce965e3b57d
+size 4197
