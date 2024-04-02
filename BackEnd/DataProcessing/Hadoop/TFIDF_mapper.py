4e23b67063e7cc79ea8d3dbb48db6f21a0223917
송강산 <rkdtks0816@naver.com>
2024-04-02 16:27:41 +0000

    feat: 데이터 형식, 저장 방법 수정
    Former-commit-id: 7852bd228ed7ef9c890612cfc2bc218256b03a88

--- BackEnd/DataProcessing/Hadoop/TFIDF_mapper.py
+++ BackEnd/DataProcessing/Hadoop/TFIDF_mapper.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:429138551b29327df8fbf4b0dc219699aecb391353f922878b670f20e055fecf
-size 1197
+oid sha256:641d716eb1480cb69b93d89487f822c85cc1bec8e98003935b76a17ec86b3d2d
+size 1203
