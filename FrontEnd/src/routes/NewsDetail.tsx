f53752889fbc211d57c7db6260700e7bd2379525
백민정 <peku9693@gmail.com>
2024-04-02 15:19:13 +0900

    fix: 빌드 오류 수정
    Former-commit-id: c0888c2cb3e549c5cba1abdf66ccf291893b63c8

--- FrontEnd/src/routes/NewsDetail.tsx
+++ FrontEnd/src/routes/NewsDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b001a3a5dc5e0753685c1a572b8408c06a00b89ee156eed9ba24b7171f39091
-size 2277
+oid sha256:d047ed0f12487830f9864dd128ac135ca29cdef49595665f65f36875b0eb86d6
+size 2227
