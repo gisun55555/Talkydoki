2b854fa8e92a71d4689afffc668883177b407a58
백민정 <peku9693@gmail.com>
2024-04-02 15:41:17 +0900

    fix:  잔버그 수정
    Former-commit-id: 0f4bd1d634428e04f804829e198d2d41f5676106

--- FrontEnd/src/api/newsApi.tsx
+++ FrontEnd/src/api/newsApi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b9214438e18efe8b79f7d324fce481c9dafed1e2567251262501730c5099bb0
-size 5849
+oid sha256:9313a982fb945eec5164077703de9f6e3efa629c12ef3250c1c01bbc063c522f
+size 5876
