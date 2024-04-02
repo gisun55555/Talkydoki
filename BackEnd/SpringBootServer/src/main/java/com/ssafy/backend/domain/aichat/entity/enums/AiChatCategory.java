488ef81a71097ebc8e8c27621245464f8bcf01d2
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-02 17:37:50 +0900

    fix: AI 회화 채팅 보고서 가져올 때 대화내역 가져오기 쿼리 수정, 카테고리 enum값 수정
    Former-commit-id: 6fbe03eee120bb473b9ea4945f8496f75a190afb

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/entity/enums/AiChatCategory.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/entity/enums/AiChatCategory.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53b7328633acc407f538d36831c1706dbb7af9bd4ee3fb2387a5eb659ec65669
-size 906
+oid sha256:ee2f70bcb6f49987232c47c98cbca63043a59296668c42def96aa49e070bce9c
+size 1157
--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/repository/OpenAiRepository.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/global/component/openai/repository/OpenAiRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8510098335503bcb351a99deda5369255a5d2fb89df4fb46a89885516b4a7846
-size 3064
+oid sha256:7791ca9b8d2ffb7fb23637df06767b2894431a54f9be3fb479a35eb7dfdff664
+size 3065
