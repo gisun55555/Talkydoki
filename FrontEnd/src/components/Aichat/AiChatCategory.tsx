474d0b2945872467c965651acc7a11fc0d981497
백민정 <peku9693@gmail.com>
2024-04-02 09:14:30 +0900

    fix: 평균점수 오류 수정
    Former-commit-id: 1e87614cd62fbb23e05ffb6bf4cce224118a430d

--- FrontEnd/src/components/Aichat/AiChatCategory.tsx
+++ FrontEnd/src/components/Aichat/AiChatCategory.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21ef8184a49251a5f9e8b80c774a37dc1f72aa66b5fb3b5ac88e9a1ce931cb6c
-size 2134
+oid sha256:11686fa868c880c3851b2e67bb8f32eaa9acaf58491ef32ac616a2813f8ffcca
+size 2184
--- FrontEnd/src/styles/Aichat/AiChatList.tsx
+++ FrontEnd/src/styles/Aichat/AiChatList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c1b04daf54250ec0fe4d4ff64b177690df03ac5d0979b9d1a12fc4808a274a1
-size 2419
+oid sha256:b821ff130a74a0bfdbe4612b3c652fd7ad326c722714c4a7110007d3d2539b64
+size 2754
