8b0a5f6c77f96814d3a101037c6479b6c0dae7d8
오기선 <pakox123@gmail.com>
2024-04-02 13:36:20 +0900

    fix: 이메일 인증 오류 메세지 추가
    Former-commit-id: fdbf64cc9accdf83eda1337301c8b69d767f0e79

--- FrontEnd/src/routes/SignUp.tsx
+++ FrontEnd/src/routes/SignUp.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7daf26debe8a7f3c04080e03a8546b4e16163659f2907be1ce9e822b795d6c8e
-size 5685
+oid sha256:90b4e55509734891dc388ca970491154a1c24fdedc3b579a22e032bfc804900b
+size 5770
