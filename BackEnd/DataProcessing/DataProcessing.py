95c370b5db5a2e0036f0f697a03e766b7ef9663e
송강산 <rkdtks0816@naver.com>
2024-04-02 12:20:25 +0000

    feat: 데이터 프로세싱 끝나는 로그 추가
    Former-commit-id: bc5da016590b76ff764f2601235e73c710342215

--- BackEnd/DataProcessing/DataProcessing.py
+++ BackEnd/DataProcessing/DataProcessing.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91bc3e9dbf6148e28f01ba4617877ec634709b223f40eec6b476e72e518c1064
-size 2417
+oid sha256:5246da3d9c6d029e998c59515eb945a4073e29288e237ba1971f2c9fd9a5f345
+size 2457
