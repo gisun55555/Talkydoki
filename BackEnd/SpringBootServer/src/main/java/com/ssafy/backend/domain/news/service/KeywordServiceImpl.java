b21af8df9e6a1e34e25255d7035ef0c4e10e9f04
이지호 <jeho1129@naver.com>
2024-04-03 09:19:21 +0900

    fix: TF-IDF 값 저장 로직 수정(Keyword 테이블에 없을 경우 Keyword 저장하고 이후 weight 저장)
    Former-commit-id: 832926687e10db426eee3e73d2482e65f8967eaa

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/KeywordServiceImpl.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/news/service/KeywordServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5111d0782e0260f6468e4306772c6dd0cfae9fafc70552d3643afcb729dad7f4
-size 3609
+oid sha256:daffb867358b9f93f01250f53c5d409eefb3cbf724f23f3013f6e95c4d37d358
+size 3807
