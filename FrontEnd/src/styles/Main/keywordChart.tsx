8f1c5cbc4ffe7131de12d508b68d5b899cbce048
이지호 <jeho1129@naver.com>
2024-04-03 16:18:20 +0900

    fix: recommendation 위한 DB 주기 3분 -> 5분으로 변경
    Former-commit-id: 6f7646d6b1108f1b1388eff5caad8c4e06a71618

--- FrontEnd/src/styles/Main/keywordChart.tsx
+++ FrontEnd/src/styles/Main/keywordChart.tsx
@@ -1,22 +1,3 @@
-import styled from "styled-components";
-import { Card } from "../common/ui/card";
-
-export const KeywordChartWrapper = styled(Card)`
-  width: 70%;
-  height: 50vh;
-  margin: 0;
-
-  .msg {
-    width: 100%;
-    height: 90%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    opacity: 0.5;
-  }
-
-  @media screen and (max-width: 992px) {
-    width: calc(100% - 3vw);
-    margin: 0;
-  }
-`;
+version https://git-lfs.github.com/spec/v1
+oid sha256:eb8aa19bb4bd1ab7e9697cb37eea021e0d1dd5e415aca4f645f232682d794378
+size 407
