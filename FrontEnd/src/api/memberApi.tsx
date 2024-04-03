a62104c1cf8b5903f6980fe817f842d5b3613a27
백민정 <peku9693@gmail.com>
2024-04-03 16:46:17 +0900

    fix: 로그아웃 버그 수정
    Former-commit-id: 34151ab129a56646dcbe97d94df9e6636baf0888

--- FrontEnd/src/api/memberApi.tsx
+++ FrontEnd/src/api/memberApi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6ed74c3c2bdcfa29a7e272d7dc0aec252c5448583459c291db1a88f379cc1a5
-size 7655
+oid sha256:5f8af6f5f712c3960af770d49d11b25c48225413ca738e5ceee727f009b22c61
+size 7843
