b6c1ca38ab351f75d6514fed8f6a74cf4cee644a
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-04 05:50:59 +0900

    fix: 회원탈퇴 긴급수정 (엔티티 AI 채팅 방 회원 엔티티간의 매핑관계)
    Former-commit-id: 52a18c5bd2088386c08ee2ac6312432811adb3ab

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/entity/AiChatRoom.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/aichat/entity/AiChatRoom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d64d836e6dc88c8d89df3c572451e7fdd6f68b950dbb8f193525e95a468925b
-size 951
+oid sha256:9b8604e49986f9a557037877283d4addd30764d4112ff5e9af684d0f7b439b7f
+size 995
