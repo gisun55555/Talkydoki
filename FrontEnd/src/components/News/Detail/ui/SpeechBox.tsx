565a01e90c571735ed03ebc7c01485c0e6c213b2
백민정 <peku9693@gmail.com>
2024-04-03 17:25:46 +0900

    fix: 사용자 키워드 업데이트 타이밍 수정
    Former-commit-id: cba1d1187de59752a11803c829b18b4c0b9df5ef

--- FrontEnd/src/components/News/Detail/ui/SpeechBox.tsx
+++ FrontEnd/src/components/News/Detail/ui/SpeechBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1d2474a2b5aae16a1b9b864cc79f79ae2a17f0b4360cdbc8427bb81facb6cc9
-size 7809
+oid sha256:6db6b7d2ea04535a11ea95cb007f8f92c92f315effceb037a2efd4e2ca2e4bde
+size 7878
