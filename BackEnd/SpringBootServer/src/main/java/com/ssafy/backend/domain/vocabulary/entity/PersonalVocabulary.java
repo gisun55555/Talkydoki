531df15475ed61ec8198eb398d14741d35cf9eaa
tlsehdrms124 <tlsehdrms124@naver.com>
2024-04-04 06:22:08 +0900

    fix: 회원탈퇴 긴급수정 최종 완료
    Former-commit-id: cd156e0a4d4bad3fd27ae60b281557b60f0f2052

--- BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/entity/PersonalVocabulary.java
+++ BackEnd/SpringBootServer/src/main/java/com/ssafy/backend/domain/vocabulary/entity/PersonalVocabulary.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a8442f17a8f21196664cc2c2e67324bd2ec2a8c4400c1fdf9dffbb5ffab7b3a
-size 2163
+oid sha256:8f8c04af3b27a16fbedd4b6d885dc439a160ccf47488a936e4f733adc7301221
+size 1976
