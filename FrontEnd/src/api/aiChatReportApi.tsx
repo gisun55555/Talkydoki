017a6bf2c37e79861fbac0a47884d5d4851be287
백민정 <peku9693@gmail.com>
2024-04-03 13:36:59 +0900

    fix: 레포트 작성 후 마이데이터 통계 바로 반영
    Former-commit-id: c45927f43bdf8d93e389f13445b65330ce232f36

--- FrontEnd/src/api/aiChatReportApi.tsx
+++ FrontEnd/src/api/aiChatReportApi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2eb02e99c2670c738b96de90ecc33b276758f3b5f9ac34d84efa1aa99b89606
-size 1015
+oid sha256:a66bb7c164406772d1f1036b68ce9e434ebbca3f98de0867ce24633bcf13a158
+size 1373
--- FrontEnd/src/api/newsApi.tsx
+++ FrontEnd/src/api/newsApi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cb460d418ec3c1970d1bd42f9dc3fc24c0c28e42269877bff94d9472650fee7
-size 5835
+oid sha256:c19a0fc8c791af5c308764bb832faea8c8484e30532745dee8241a099eb8cfc4
+size 5808
